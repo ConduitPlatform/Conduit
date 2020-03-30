3d5b5676be87c6dca995da93f6847eaf7a8e0277
John Lamis <lamisjohn@gmail.com>
2020-03-30 19:53:38 +0300

    implement: sendEmail function fix: initialization to correctly pass the transport and config values

--- packages/email-provider/index.js
+++ packages/email-provider/index.js
@@ -1,9 +1,61 @@
 const emailProvider = require('@conduit/email-provider');
+const {isNil} = require('lodash');
 
 let emailer;
 
-function sendMail() {
+function sendMail(params) {
+    const {
+        subject,
+        body,
+        email,
+        variables,
+        sender
+    } = params;
 
+    if (isNil(emailer)) {
+        throw new Error("Module not initialized");
+    }
+
+    const builder = emailer.emailBuilder();
+    if (isNil(builder)) {
+        return;
+    }
+
+    if (isNil(subject)) {
+        throw new Error("Cannot send email without subject");
+    }
+
+    if (isNil(email)) {
+        throw new Error("Cannot send email without receiver");
+    }
+
+    if (isNil(sender)) {
+        throw new Error("Cannot send email without a sender");
+    }
+
+    const bodyString = replaceVars(body, variables);
+
+    builder.setSender(sender);
+    builder.setContent(bodyString);
+    builder.setReceiver(email);
+    builder.setSubject(subject);
+
+    return emailer.sendEmail(builder);
+}
+
+function replaceVars(body, variables) {
+    let str = body;
+    Object.keys(variables).forEach(key => {
+        const regex = new RegExp(`{{${key}}}`, 'g');
+        let value = variables[key];
+        if (Array.isArray(value)) {
+            value = value.toString();
+        } else if (typeof value === 'object') {
+            value = JSON.stringify(value);
+        }
+        str = str.replace(regex, value);
+    });
+    return str;
 }
 
 
@@ -15,9 +67,12 @@ function initialize(app, config) {
     if (!app) {
         throw new Error("No app provided")
     }
-    // initialize properly
-    emailer = new emailProvider.EmailProvider('mailgun', {});
+    const {transport, transportSettings} = config;
+    if (isNil(transport) || isNil(transportSettings)) {
+        throw new Error("You need to provide both a transport and a config");
+    }
 
+    emailer = new emailProvider.EmailProvider(transport, transportSettings);
     return true;
 
 }
