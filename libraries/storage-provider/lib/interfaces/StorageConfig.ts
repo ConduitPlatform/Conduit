cac4e6263a97bea9039d6d116d53c0055c9b7f6a
kkopanidis <kkopanidis@gmail.com>
2020-04-03 20:51:38 +0300

    add: basic local storage provider todo: add more functions

--- libraries/storage-provider/lib/interfaces/StorageConfig.ts
+++ libraries/storage-provider/lib/interfaces/StorageConfig.ts
@@ -1,6 +1,10 @@
 export interface StorageConfig {
 
-
+    /**
+     * The path to use for storing local files
+     * This is only used with the local storage provider
+     */
+    storagePath: string;
     google: {
         serviceAccountKeyPath: string,
         bucketName: string
--- libraries/storage-provider/lib/providers/local/index.ts
+++ libraries/storage-provider/lib/providers/local/index.ts
@@ -1,13 +1,39 @@
 import {IStorageProvider} from "../../interfaces/IStorageProvider";
+import {StorageConfig} from "../../interfaces/StorageConfig";
+import {existsSync, writeFile, readFile} from "fs";
+import {resolve} from "path";
 
 export class LocalStorage implements IStorageProvider {
 
+    _storagePath: string;
+
+    constructor(options: StorageConfig) {
+        this._storagePath = options && options.storagePath ? options.storagePath : ''
+    }
+
     get(fileName: string): any {
+        if (!existsSync(resolve(this._storagePath, fileName))) {
+            return new Promise(function (res, reject) {
+                reject("File does not exist");
+            });
+        }
+        const self = this;
+        return new Promise(function (res, reject) {
+            readFile(resolve(self._storagePath, fileName), function (err) {
+                if (err) reject(err);
+                else res(true);
+            });
+        });
+
     }
 
     store(fileName: string, data: any): Promise<boolean | Error> {
-        return new Promise((resolve) => {
-            resolve();
+        const self = this;
+        return new Promise(function (res, reject) {
+            writeFile(resolve(self._storagePath, fileName), data, function (err) {
+                if (err) reject(err);
+                else res(true);
+            });
         });
     }
 
