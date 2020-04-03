71e976dbed473e6da87ad67bbe61ed657f6d4610
kkopanidis <kkopanidis@gmail.com>
2020-04-03 15:14:45 +0300

    add: new in-memory store module add: redis provider add: local provider add: memcached provider

---
+++ packages/in-memory-store/lib/index.ts
@@ -0,0 +1,44 @@
+import {LocalSettings} from "./interaces/LocalSettings";
+import {RedisSettings} from "./interaces/RedisSettings";
+import {MemcachedSettings} from "./interaces/MemcachedSettings";
+import {RedisProvider} from "./providers/redis";
+import {Localprovider} from "./providers/local";
+import {MemcachedProvider} from "./providers/memcached";
+import {StorageProvider} from "./interaces/StorageProvider";
+
+export class InMemoryStore implements StorageProvider {
+
+    private static _instance: InMemoryStore;
+    _provider: StorageProvider;
+
+    private constructor(name: string, storageSettings: LocalSettings | RedisSettings | MemcachedSettings) {
+        if (name === 'redis') {
+            this._provider = new RedisProvider(storageSettings as RedisSettings);
+        } else if (name === 'memcache') {
+            this._provider = new MemcachedProvider(storageSettings as MemcachedSettings);
+        } else {
+            this._provider = new Localprovider(storageSettings as LocalSettings);
+        }
+    }
+
+    public static getInstance(name?: string, storageSettings?: LocalSettings | RedisSettings | MemcachedSettings) {
+        if (!this._instance && name && storageSettings) {
+            this._instance = new InMemoryStore(name, storageSettings);
+        } else if (this._instance) {
+            return this._instance
+        } else {
+            throw new Error("No settings provided to initialize");
+        }
+    }
+
+    get(key: string): Promise<any> {
+        return this._provider.get(key);
+    }
+
+    store(key: string, value: any): Promise<any> {
+        return this._provider.store(key, value);
+    }
+}
+
+
+
