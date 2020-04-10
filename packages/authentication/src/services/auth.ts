3e9a557a0302f9e1198daedf5d1ec10e766f3a3d
John Lamis <lamisjohn@gmail.com>
2020-04-10 03:35:52 +0300

    add: auth service

---
+++ packages/authentication/src/services/auth.ts
@@ -0,0 +1,35 @@
+import * as crypto from 'crypto';
+import { ISignTokenOptions } from '../interfaces/ISignTokenOptions';
+import * as jwt from 'jsonwebtoken';
+import * as bcrypt from 'bcrypt';
+
+export class AuthService {
+  constructor(
+  ) {
+  }
+
+  randomToken() {
+    return crypto.randomBytes(64).toString('base64');
+  }
+
+  signToken(data: { [key: string]: any }, options: ISignTokenOptions) {
+    const { secret, expiresIn } = options;
+    return jwt.sign(data, secret, { expiresIn });
+  }
+
+  verify(token: string, secret: string) {
+    try {
+      return jwt.verify(token, secret);
+    } catch (error) {
+      return null;
+    }
+  }
+
+  async hashPassword(password: string, hashRounds = 10) {
+    return bcrypt.hash(password, hashRounds);
+  }
+
+  async checkPassword(password: string, hashed: string) {
+    return bcrypt.compare(password, hashed);
+  }
+}
