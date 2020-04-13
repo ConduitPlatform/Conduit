022ab6b3161d1eb31946a268309c91c23bb54d48
john <comrade.9510@gmail.com>
2020-04-13 19:26:04 +0300

    storage

--- admin/Conduit-app/src/pages/storage.js
+++ admin/Conduit-app/src/pages/storage.js
@@ -4,6 +4,7 @@ import Typography from "@material-ui/core/Typography";
 import CustomTabs from "../components/CustomTabs";
 import React, {useState} from "react";
 import {privateRoute} from "../components/utils/privateRoute";
+import StorageFiles from "../components/StorageFiles";
 
 const tabs = ['Files', 'Settings'];
 
@@ -20,6 +21,7 @@ const Storage = () => {
 				<Typography variant={"h4"}>Storage</Typography>
 				<CustomTabs tabs={tabs} selected={selected} handleChange={handleChange}/>
 				<Box role="tabpanel" hidden={selected !== 0} id={`tabpanel-0`}>
+					<StorageFiles/>
 				</Box>
 			</Box>
 		</Layout>
