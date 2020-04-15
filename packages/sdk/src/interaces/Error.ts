cef9a122f5f1e48c37c9848080225a77a13cc7eb
Chris <capostolidis1@gmail.com>
2020-04-15 15:48:32 +0300

    add: static methods on ConduitError that return preconstructed ConduitError objects for unauthorized, notFound, forbidden and userInput errors

--- packages/sdk/src/interaces/Error.ts
+++ packages/sdk/src/interaces/Error.ts
@@ -10,6 +10,21 @@ export class ConduitError extends Error {
         this.status = status;
     }
 
+    static unauthorized(message: string = 'Unauthorized') {
+        return new ConduitError('UNAUTHORIZED', 401, message);
+    }
+
+    static notFound(message: string = 'Resource not found') {
+        return new ConduitError('NOT_FOUND', 404, message);
+    }
+
+    static forbidden(message: string = 'Forbidden') {
+        return new ConduitError('FORBIDDEN', 403, message);
+    }
+
+    static userInput(message: string = 'Request data are invalid') {
+        return new ConduitError('USER_INPUT_ERROR', 400, message);
+    }
 }
 
 export class UnauthorizedError extends ConduitError {
