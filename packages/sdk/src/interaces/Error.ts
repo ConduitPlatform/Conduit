20532ca8f8bd6fb4001e80ed315d0d07ca298618
Chris <capostolidis1@gmail.com>
2020-04-15 13:30:19 +0300

    add: specific error classes with appropriate name, status, message? that extend Conduit error

--- packages/sdk/src/interaces/Error.ts
+++ packages/sdk/src/interaces/Error.ts
@@ -11,3 +11,27 @@ export class ConduitError extends Error {
     }
 
 }
+
+export class UnauthorizedError extends ConduitError {
+    constructor(message: string = 'Unauthorized') {
+        super('UNAUTHORIZED', 401, message);
+    }
+}
+
+export class NotFoundError extends ConduitError {
+    constructor(message: string = 'Resource not found') {
+        super('NOT_FOUND', 404, message);
+    }
+}
+
+export class ForbiddenError extends ConduitError {
+    constructor(message: string = 'Forbidden') {
+        super('FORBIDDEN', 403, message);
+    }
+}
+
+export class UserInputError extends ConduitError {
+    constructor(message: string = 'Request data are invalid') {
+        super('USER_INPUT_ERROR', 422, message);
+    }
+}
