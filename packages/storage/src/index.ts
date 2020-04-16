ef40dd25db7a8e21bfcd24ca9217348630ea9e0e
John Lamis <lamisjohn@gmail.com>
2020-04-16 20:20:58 +0300

    update: tsconfig

--- packages/storage/src/index.ts
+++ packages/storage/src/index.ts
@@ -1,32 +1,31 @@
-import {NextFunction, Request, Response} from 'express';
-import {createStorageProvider, IStorageProvider} from '@conduit/storage-provider';
-import {ConduitSDK, IConduitAdmin, IConduitStorage} from '@conduit/sdk';
-import {AdminConfigHandlers} from "./admin/config";
+import { NextFunction, Request, Response } from 'express';
+import { createStorageProvider, IStorageProvider } from '@conduit/storage-provider';
+import { AdminConfigHandlers } from './admin/config';
+import { ConduitSDK, IConduitAdmin, IConduitStorage } from '@conduit/sdk';
 
-class StorageModule extends IConduitStorage {
-    private readonly storageProvider: IStorageProvider;
+class StorageModule extends IConduitStorage{
+  private readonly storageProvider: IStorageProvider;
 
-    constructor(conduit: ConduitSDK) {
-        super(conduit);
+  constructor(conduit: ConduitSDK) {
+    super(conduit);
 
-        const config = (conduit as any).config;
-        const storageConfig = config.get('storage');
+    const config = (conduit as any).config;
+    const storageConfig = config.get('storage');
 
-        const {provider, storagePath, google} = storageConfig;
+    const { provider, storagePath, google } = storageConfig;
 
-        this.storageProvider = createStorageProvider(provider, {storagePath, google});
+    this.storageProvider = createStorageProvider(provider, { storagePath, google });
 
-        const admin = conduit.getAdmin();
-        const adminHandlers = new AdminConfigHandlers(conduit);
+    const admin = conduit.getAdmin();
+    const adminHandlers = new AdminConfigHandlers(conduit);
 
-        this.registerAdminRoutes(admin, adminHandlers);
+    this.registerAdminRoutes(admin, adminHandlers);
+  }
 
-    }
-
-    private registerAdminRoutes(admin: IConduitAdmin, adminHandlers: AdminConfigHandlers) {
-        admin.registerRoute('PUT', '/storage/config', (req: Request, res: Response, next: NextFunction) => adminHandlers.editConfig(req, res).catch(next));
-        admin.registerRoute('GET', '/storage/config', (req: Request, res: Response, next: NextFunction) => adminHandlers.getConfig(req, res).catch(next));
-    }
+  private registerAdminRoutes(admin: IConduitAdmin, adminHandlers: AdminConfigHandlers) {
+    admin.registerRoute('PUT', '/storage/config', (req: Request, res: Response, next: NextFunction) => adminHandlers.editConfig(req, res).catch(next));
+    admin.registerRoute('GET', '/storage/config', (req: Request, res: Response, next: NextFunction) => adminHandlers.getConfig(req, res).catch(next));
+  }
 }
 
 export = StorageModule;
