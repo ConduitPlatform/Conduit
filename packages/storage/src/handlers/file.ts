f14077e1e7bf8b3e7d6f052f91c0b5d00e062985
John Lamis <lamisjohn@gmail.com>
2020-04-21 13:36:59 +0300

    add: handlers to delete and update a file with rollback logic

--- packages/storage/src/handlers/file.ts
+++ packages/storage/src/handlers/file.ts
@@ -1,6 +1,7 @@
 import { ConduitError, ConduitRouteParameters, ConduitSDK, IConduitDatabase } from '@conduit/sdk';
 import { isString, isNil } from 'lodash';
 import { IStorageProvider } from '@conduit/storage-provider';
+import { v4 as uuid } from 'uuid';
 
 export class FileHandlers {
   private readonly database: IConduitDatabase;
@@ -29,11 +30,20 @@ export class FileHandlers {
       throw ConduitError.userInput('Invalid data provided');
     }
 
+    if (!isString(folder)) {
+      throw ConduitError.userInput('No folder provided');
+    }
+
     const buffer = Buffer.from(data, 'base64');
 
-    const file = await File.create({ name, mimeType, user, folder });
+    const exists = await this.storageProvider.folder(folder).exists('');
+    if (!exists) {
+      await this.storageProvider.folder('').createFolder(folder);
+    }
+
     await this.storageProvider.folder(folder).store(name, buffer);
-    return file;
+
+    return File.create({ name, mimeType, user, folder });
   }
 
   async getFile(params: ConduitRouteParameters) {
@@ -67,5 +77,101 @@ export class FileHandlers {
       ...found,
       data
     };
+  }
+
+  async deleteFile(params: ConduitRouteParameters) {
+    const id = params.params?.id;
+    const user = params.context?.user;
+    if (isNil(user)) {
+      throw ConduitError.forbidden('Invalid credentials');
+    }
+    if (!isString(id)) {
+      throw ConduitError.userInput('The provided id is invalid');
+    }
+    const File = this.database.getSchema('File');
+    const found = await File.findOne({ _id: id, user: user._id });
+    if (isNil(found)) {
+      throw ConduitError.notFound('File not found');
+    }
+
+    const success = await this.storageProvider.folder(found.folder).delete(found.name);
+    if (!success) {
+      throw ConduitError.internalServerError('Error deleting the file');
+    }
+
+    await File.deleteOne({ _id: id });
+
+    return { success: true };
+  }
+
+  async updateFile(params: ConduitRouteParameters) {
+    const id = params.params?.id;
+    const user = params.context?.user;
+    if (isNil(user)) {
+      throw ConduitError.forbidden('Invalid credentials');
+    }
+    if (!isString(id)) {
+      throw ConduitError.userInput('The provided id is invalid');
+    }
+    const File = this.database.getSchema('File');
+    const found = await File.findOne({ _id: id, user: user._id });
+    if (isNil(found)) {
+      throw ConduitError.notFound('File not found');
+    }
+
+    // Create temporary file to make the changes so the original is not corrupted if anything fails
+    const tempFileName = uuid();
+    const oldData = await this.storageProvider.folder(found.folder).get(found.name)
+      .catch(error => {
+        throw ConduitError.internalServerError('Error reading file');
+      });
+
+    const exists = await this.storageProvider.folder('temp').exists('');
+    if (!exists) {
+      await this.storageProvider.folder('temp').createFolder('');
+    }
+
+    await this.storageProvider.folder('temp').store(tempFileName, oldData)
+      .catch(error => {
+        console.log(error);
+        throw ConduitError.internalServerError('I/O error');
+      });
+
+    let failed = false;
+
+    const data = params.params?.data;
+    if (!isNil(data)) {
+      const buffer = Buffer.from(data, 'base64');
+      await this.storageProvider.folder('temp').store(tempFileName, buffer)
+        .catch(error => {
+          failed = true;
+        });
+    }
+
+    const newName = params.params?.name ?? found.name;
+    const newFolder = params.params?.folder ?? found.folder;
+
+    const shouldRemove = newName !== found.name;
+
+    found.mimeType = params.params?.mimeType ?? found.mimeType;
+
+    // Commit the changes to the actual file if everything is successful
+    if (failed) {
+      await this.storageProvider.folder('temp').delete(tempFileName);
+      throw ConduitError.internalServerError();
+    }
+    await this.storageProvider.folder('temp').moveToFolderAndRename(tempFileName, newName, newFolder)
+      .catch(error => {
+        throw ConduitError.internalServerError('I/O error');
+      });
+
+    if (shouldRemove) {
+      await this.storageProvider.folder(found.folder).delete(found.name);
+    }
+
+    found.name = newName;
+    found.folder = newFolder;
+
+    return File.findByIdAndUpdate(found);
   }
 }
