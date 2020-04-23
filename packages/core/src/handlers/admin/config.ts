2b81ac9fd500753fcb4a36f94c817e37d2e21c08
Chris <capostolidis1@gmail.com>
2020-04-23 12:17:58 +0300

    add: setConfig handler on core

--- packages/core/src/handlers/admin/config.ts
+++ packages/core/src/handlers/admin/config.ts
@@ -50,63 +50,60 @@ export class ConfigAdminHandlers {
 
     return res.json(finalConfig);
   }
-}
 
-//   async setConfig(req: Request, res: Response) {
-//     const newConfig = req.body;
-//
-//     const Config = this.database.getSchema('Config');
-//
-//     const dbConfig = await Config.findOne({});
-//     if (isNil(dbConfig)) {
-//       return res.status(404).json({ error: 'Config not set' });
-//     }
-//
-//     const appConfig = (this.sdk as any).config as ConvictConfig<any>;
-//     const currentConfig = appConfig.get();
-//     const final = merge(currentConfig, newConfig);
-//
-//     Object.assign(dbConfig, final);
-//     const saved = await Config.findByIdAndUpdate(dbConfig);
-//     delete saved._id;
-//     delete saved.createdAt;
-//     delete saved.updatedAt;
-//     delete saved.__v;
-//
-//     appConfig.load(saved);
-//
-//     return res.json(saved);
-//   }
-//
-//   async editInMemoryStoreConfig(req: Request, res: Response) {
-//     const Config = this.database.getSchema('Config');
-//     const appConfig = (this.sdk as any).config;
-//     const newInMemoryStoreConfig = req.body;
-//     if (!isNil(newInMemoryStoreConfig.active)) {
-//       delete newInMemoryStoreConfig.active;
-//     }
-//
-//     const dbConfig = await Config.findOne({});
-//     if (isNil(dbConfig)) {
-//       return res.status(404).json({ error: 'Config not set' });
-//     }
-//
-//
-//     if (!this.sdk.getInMemoryStore().validateConfig(newInMemoryStoreConfig)) {
-//       return res.status(403).json({error: 'Invalid Configuration given'});
-//     }
-//
-//     const final = merge(currentInMemoryStoreConfig, newInMemoryStoreConfig);
-//     dbConfig.inMemoryStore = final;
-//     const saved = await Config.findByIdAndUpdate(dbConfig);
-//     delete saved._id;
-//     delete saved.createdAt;
-//     delete saved.updatedAt;
-//     delete saved.__v;
-//     await appConfig.load(saved);
-//
-//
-//
-//     return res.json(saved.email);
-//   }
-// }
+  async setConfig(req: Request, res: Response) {
+    const Config = this.database.getSchema('Config');
+    const dbConfig = await Config.findOne({});
+    if (isNil(dbConfig)) {
+      return res.status(404).json({ error: 'Config not set' });
+    }
+    const appConfig = (this.sdk as any).config as ConvictConfig<any>;
+
+    const newConfig = req.body;
+    const moduleName = req.params.module;
+    let module: any;
+    let currentConfig: any;
+    let configProperty: string;
+
+    switch (moduleName) {
+      case undefined:
+        currentConfig = dbConfig;
+        break;
+      case 'in-memory-store':
+        currentConfig = dbConfig.inMemoryStore;
+        module = this.sdk.getInMemoryStore();
+        configProperty = 'inMemoryStore';
+        break;
+      default:
+        return res.status(403).json({ error: 'Invalid module name' });
+    }
+
+    // Validate here
+    if (!isNil(module) && !module.validateConfig(newConfig)) { // General config doesn't get validated
+      return res.status(403).json({ error: 'Invalid configuration structure' });
+    }
+
+    if (isNil(currentConfig)) currentConfig = {};
+    const final = merge(currentConfig, newConfig);
+    if (isNil(module)){
+      Object.assign(dbConfig, final);
+    } else {
+      if (isNil(dbConfig[configProperty!])) dbConfig[configProperty!] = {};
+      Object.assign(dbConfig[configProperty!], final);
+    }
+    const saved = await Config.findByIdAndUpdate(dbConfig);
+    delete saved._id;
+    delete saved.createdAt;
+    delete saved.updatedAt;
+    delete saved.__v;
+    appConfig.load(saved);
+
+    // Enable module here
+    if (module && ! await module.initModule()) {
+      return res.status(403).json({ error: 'Invalid configuration values' });
+    }
+
+    if (isNil(module)) return res.json(saved);
+    return res.json(saved[configProperty!]);
+  }
+}
