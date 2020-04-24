b115362d820992bd0e2914b46b6a050b152da0d3
Chris <capostolidis1@gmail.com>
2020-04-24 17:35:14 +0300

    fix: refactored inMemoryStore module to use setConfig

--- packages/in-memory-store/src/index.ts
+++ packages/in-memory-store/src/index.ts
@@ -22,43 +22,42 @@ class InMemoryStore extends IConduitInMemoryStore implements StorageProvider {
         }
     }
 
-    async initModule() {
-        try {
-            if ((this.conduit as any).config.get('inMemoryStore.active')) {
-                if (this.isRunning) return {result: true};
-                await this.enableModule();
-                return {result: true};
-            }
-            throw new Error('Module is not active');
-        } catch (e){
-            console.log(e);
-            return {result: false, error: e};
+
+    async setConfig(newConfig: any) {
+        if (!ConduitSDK.validateConfig(newConfig, InMemoryStoreConfigSchema.inMemoryStore)) {
+            throw new Error('Invalid configuration values');
         }
+
+        let errorMessage: string | null = null;
+        const updateResult = await this.conduit.updateConfig(newConfig, 'inMemoryStore').catch((e: Error) => errorMessage = e.message);
+        if (!isNil(errorMessage)) {
+            throw new Error(errorMessage);
+        }
+
+        if ((this.conduit as any).config.get('inMemoryStore.active')) {
+            await this.enableModule().catch((e: Error) => errorMessage = e.message);
+        } else {
+            throw new Error('Module is not active');
+        }
+        if (!isNil(errorMessage)) {
+            throw new Error(errorMessage);
+        }
+
+        return updateResult;
     }
 
     private async enableModule() {
-        const name = (this.conduit as any).config.get('inMemoryStore.providerName');
-        const storageSettings: LocalSettings | RedisSettings | MemcachedSettings = (this.conduit as any).config.get(`inMemoryStore.settings.${name}`);
-
-        if (name === 'redis') {
-            this._provider = new RedisProvider(storageSettings as RedisSettings);
-            const isReady = await (this._provider as RedisProvider).isReady();
-            if (!isReady) throw new Error('Redis failed to connect');
-        } else if (name === 'memcache') {
-            this._provider = new MemcachedProvider(storageSettings as MemcachedSettings);
+        if (!this.isRunning) {
+            await this.initProvider();
+            const admin = this.conduit.getAdmin();
+            admin.registerRoute('POST', '/in-memory-store',
+              (req: Request, res: Response, next: NextFunction) => this.adminStore(req, res, next).catch(next));
+            admin.registerRoute('GET', '/in-memory-store/:key',
+              (req: Request, res: Response, next: NextFunction) => this.adminGetByKey(req, res, next).catch(next));
+            this.isRunning = true;
         } else {
-            this._provider = new Localprovider(storageSettings as LocalSettings);
+            await this.initProvider();
         }
-
-        const admin = this.conduit.getAdmin();
-
-        admin.registerRoute('POST', '/in-memory-store',
-          (req: Request, res: Response, next: NextFunction) => this.adminStore(req, res, next).catch(next));
-
-        admin.registerRoute('GET', '/in-memory-store/:key',
-          (req: Request, res: Response, next: NextFunction) => this.adminGetByKey(req, res, next).catch(next));
-
-        this.isRunning = true;
     }
 
     static get config() {
@@ -71,6 +70,20 @@ class InMemoryStore extends IConduitInMemoryStore implements StorageProvider {
 
     store(key: string, value: any): Promise<any> {
         return this._provider!.store(key, value);
+    }
+
+    private async initProvider() {
+        const name = (this.conduit as any).config.get('inMemoryStore.providerName');
+        const storageSettings: LocalSettings | RedisSettings | MemcachedSettings = (this.conduit as any).config.get(`inMemoryStore.settings.${name}`);
+        if (name === 'redis') {
+            this._provider = new RedisProvider(storageSettings as RedisSettings);
+            const isReady = await (this._provider as RedisProvider).isReady();
+            if (!isReady) throw new Error('Redis failed to connect');
+        } else if (name === 'memcache') {
+            this._provider = new MemcachedProvider(storageSettings as MemcachedSettings);
+        } else {
+            this._provider = new Localprovider(storageSettings as LocalSettings);
+        }
     }
 
     private async adminStore(req: Request, res: Response, next: NextFunction) {
