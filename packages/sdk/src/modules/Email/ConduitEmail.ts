56a1143af6317bb5d2dec3a144b81974becaade0
Chris <capostolidis1@gmail.com>
2020-04-24 16:09:24 +0300

    add: new IConduitModule interface that has the setConfig function and gets implemented by modules

--- packages/sdk/src/modules/Email/ConduitEmail.ts
+++ packages/sdk/src/modules/Email/ConduitEmail.ts
@@ -1,11 +1,10 @@
-import { ConduitSDK } from '../../index';
+import { ConduitSDK, IConduitModule } from '../../index';
 import { ISendEmailParams, IRegisterTemplateParams } from './interfaces';
 
 export abstract class IConduitEmail {
   constructor(conduit: ConduitSDK) {
   }
 
-  abstract setConfig(newConfig: any): Promise<any>;
   abstract sendEmail(template: string, params: ISendEmailParams): Promise<any>;
   abstract registerTemplate(params: IRegisterTemplateParams): Promise<any>;
 }
