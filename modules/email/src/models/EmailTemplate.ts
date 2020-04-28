ad44534547bf75e1a2305580a98ff672200c008e
kkopanidis <kkopanidis@gmail.com>
2020-04-28 17:55:39 +0300

    add: demo grpc

--- modules/email/src/models/EmailTemplate.ts
+++ modules/email/src/models/EmailTemplate.ts
@@ -1,32 +1,32 @@
-import { ConduitSchema, TYPE } from '@conduit/sdk';
+import {ConduitSchema, TYPE} from '@conduit/sdk';
 
 export const emailTemplateSchema = new ConduitSchema('EmailTemplate',
-  {
-    _id: TYPE.ObjectId,
-    name: {
-      type: TYPE.String,
-      unique: true,
-      required: true
+    {
+        _id: TYPE.ObjectId,
+        name: {
+            type: TYPE.String,
+            unique: true,
+            required: true
+        },
+        subject: {
+            type: TYPE.String
+        },
+        body: {
+            type: TYPE.String,
+            required: true
+        },
+        variables: {
+            type: [String]
+        },
+        createdAt: {
+            type: TYPE.Date,
+            required: true
+        },
+        updatedAt: {
+            type: TYPE.Date,
+            required: true
+        }
     },
-    subject: {
-      type: TYPE.String
-    },
-    body: {
-      type: TYPE.String,
-      required: true
-    },
-    variables: {
-      type: [String]
-    },
-    createdAt: {
-      type: TYPE.Date,
-      required: true
-    },
-    updatedAt: {
-      type: TYPE.Date,
-      required: true
-    }
-  },
-  {
-    timestamps: true
-  });
+    {
+        timestamps: true
+    });
