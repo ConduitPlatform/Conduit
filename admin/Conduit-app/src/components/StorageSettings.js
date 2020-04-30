e8e9ea99c7ff408ca42276bbdcf2e38680af1072
dimking <dimitris.vasileias@gmail.com>
2020-04-30 16:44:14 +0300

    Add: configuration page | storage page

--- admin/Conduit-app/src/components/StorageSettings.js
+++ admin/Conduit-app/src/components/StorageSettings.js
@@ -1,20 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import { Container, FormControl, InputLabel, MenuItem, Select } from "@material-ui/core";
-import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Switch from "@material-ui/core/Switch";
-import Divider from "@material-ui/core/Divider";
-import TextField from "@material-ui/core/TextField";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import IconButton from "@material-ui/core/IconButton";
-import { Visibility, VisibilityOff } from "@material-ui/icons";
-import Button from "@material-ui/core/Button";
-import clsx from "clsx";
-import { Theme as theme } from "@material-ui/core/styles/createMuiTheme";
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { Container, FormControl, InputLabel, MenuItem, Select } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import Divider from '@material-ui/core/Divider';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -31,13 +26,6 @@ const useStyles = makeStyles((theme) => ({
   },
   formControl: {
     minWidth: 250,
-    marginRight: theme.spacing(4),
-  },
-  textField: {
-    marginBottom: theme.spacing(2),
-  },
-  simpleTextField: {
-    width: '65ch',
   },
 }));
 
@@ -50,9 +38,8 @@ const StorageSettings = ({ config, handleSave, ...rest }) => {
     google: {
       serviceAccountKeyPath: '~/google_storage_service_account.json',
       bucketName: 'conduit',
-    }
+    },
   });
-
 
   useEffect(() => {
     if (!config) {
@@ -66,9 +53,9 @@ const StorageSettings = ({ config, handleSave, ...rest }) => {
       google: {
         serviceAccountKeyPath: config.google.serviceAccountKeyPath,
         bucketName: config.google.bucketName,
-      }
+      },
     });
-  }, []);
+  }, [config]);
 
   const handleSelect = (event) => {
     setSettingsSate({
@@ -78,16 +65,28 @@ const StorageSettings = ({ config, handleSave, ...rest }) => {
   };
 
   const handleCancel = () => {
-    setSettingsSate({
-      ...settingsState,
-      active: config.active,
-      provider: config.provider,
-      storagePath: config.storagePath,
-      google: {
-        serviceAccountKeyPath: config.google.serviceAccountKeyPath,
-        bucketName: config.google.bucketName,
-      }
-    });
+    if (config) {
+      setSettingsSate({
+        ...settingsState,
+        active: config.active,
+        provider: config.provider,
+        storagePath: config.storagePath,
+        google: {
+          serviceAccountKeyPath: config.google.serviceAccountKeyPath,
+          bucketName: config.google.bucketName,
+        },
+      });
+    } else {
+      setSettingsSate({
+        active: true,
+        provider: 'local',
+        storagePath: '/var/tmp',
+        google: {
+          serviceAccountKeyPath: '~/google_storage_service_account.json',
+          bucketName: 'conduit',
+        },
+      });
+    }
   };
   const save = () => {
     const data = {
@@ -98,128 +97,128 @@ const StorageSettings = ({ config, handleSave, ...rest }) => {
 
   const renderSettingsFields = () => {
     return (
-        <>
-          <Grid item xs={ 12 }>
-            <Typography variant={ 'h6' }>The provider to use for storage</Typography>
-          </Grid>
-          <Grid item xs={ 6 }>
-            <FormControl variant="outlined" className={ classes.formControl }>
-              <InputLabel>Provider</InputLabel>
-              <Select
-                  required
-                  labelId="provider-outlined-label"
-                  value={ settingsState.provider }
-                  onChange={ handleSelect }
-                  label="Provider">
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value={ 'local' }> Local </MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={ 6 }>
-            <TextField
-                onChange={ (event) => {
-                  setSettingsSate({
-                    ...settingsState,
-                    storagePath: event.target.value,
-                  });
-                } }
-                value={ settingsState.storagePath }
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                name="storagePath"
-                label="Storage path"
-                type="text"
-                id="storagePath"
-                autoComplete="storagePath"
-                className={ clsx(classes.textField, classes.simpleTextField) }
-            />
-          </Grid>
-          <Divider className={ classes.divider }/>
-          <Grid item xs={ 12 }>
-            <Typography variant={ 'h6' }>The config for the Google storage provide</Typography>
-          </Grid>
-          <Box className={ classes.myForm }>
-            <TextField
-                id="serviceAccountKeyPath"
-                type="text"
-                label="Account key path"
-                variant="outlined"
-                value={ settingsState.google.serviceAccountKeyPath }
-                onChange={ (event) => {
-                  setSettingsSate({
-                    ...settingsState,
-                    google: {
-                      serviceAccountKeyPath: event.target.value
-                    },
-                  });
-                } }
-                className={ clsx(classes.textField, classes.simpleTextField) }
-            />
-            <TextField
-                id="bucketName"
-                type="text"
-                label="Bucket name"
-                variant="outlined"
-                value={ settingsState.google.bucketName }
-                onChange={ (event) => {
-                  setSettingsSate({
-                    ...settingsState,
-                    google: {
-                      bucketName: event.target.value
-                    },
-                  });
-                } }
-                className={ clsx(classes.textField, classes.simpleTextField) }
-            />
-          </Box>
-        </>
+      <>
+        <Grid item xs={12}>
+          <Typography variant={'h6'}>The provider to use for storage</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel>Provider</InputLabel>
+            <Select
+              required
+              labelId="provider-outlined-label"
+              value={settingsState.provider}
+              onChange={handleSelect}
+              label="Provider">
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={'local'}> Local </MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            onChange={(event) => {
+              setSettingsSate({
+                ...settingsState,
+                storagePath: event.target.value,
+              });
+            }}
+            value={settingsState.storagePath}
+            variant="outlined"
+            required
+            fullWidth
+            name="storagePath"
+            label="Storage path"
+            type="text"
+            id="storagePath"
+            autoComplete="storagePath"
+          />
+        </Grid>
+        <Divider className={classes.divider} />
+        <Grid item xs={12}>
+          <Typography variant={'h6'}>The config for the Google storage provide</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            id="serviceAccountKeyPath"
+            type="text"
+            label="Account key path"
+            variant="outlined"
+            fullWidth
+            value={settingsState.google.serviceAccountKeyPath}
+            onChange={(event) => {
+              setSettingsSate({
+                ...settingsState,
+                google: {
+                  serviceAccountKeyPath: event.target.value,
+                },
+              });
+            }}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            id="bucketName"
+            type="text"
+            label="Bucket name"
+            variant="outlined"
+            fullWidth
+            value={settingsState.google.bucketName}
+            onChange={(event) => {
+              setSettingsSate({
+                ...settingsState,
+                google: {
+                  bucketName: event.target.value,
+                },
+              });
+            }}
+          />
+        </Grid>
+      </>
     );
   };
 
   return (
-      <Container>
-        <Paper className={ classes.paper }>
-          <Grid container>
-            <Box width={ '100%' } display={ 'inline-flex' } justifyContent={ 'space-between' } alignItems={ 'center' }>
-              <Typography variant={ 'h6' }>Activate Storage Module</Typography>
-              <FormControlLabel
-                  control={
-                    <Switch
-                        checked={ settingsState.active }
-                        onChange={ () =>
-                            setSettingsSate({
-                              ...settingsState,
-                              active: !settingsState.active,
-                            })
-                        }
-                        value={ 'accountLinking' }
-                        color="primary"
-                    />
+    <Container>
+      <Paper className={classes.paper}>
+        <Grid container>
+          <Box width={'100%'} display={'inline-flex'} justifyContent={'space-between'} alignItems={'center'}>
+            <Typography variant={'h6'}>Activate Storage Module</Typography>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={settingsState.active}
+                  onChange={() =>
+                    setSettingsSate({
+                      ...settingsState,
+                      active: !settingsState.active,
+                    })
                   }
-                  label={ '' }
-              />
-            </Box>
-            <Divider className={ classes.divider }/>
-            <Grid container spacing={ 2 } className={ classes.innerGrid }>
-              { settingsState.active && renderSettingsFields() }
-            </Grid>
-            <Grid item container xs={ 12 } justify={ 'flex-end' } style={ { marginTop: 16 } }>
-              <Button onClick={ () => handleCancel() } style={ { marginRight: 16 } } color={ 'primary' }>
-                Cancel
-              </Button>
-              <Button variant="contained" color="primary" style={ { alignSelf: 'flex-end' } } onClick={ () => save() }>
-                Save
-              </Button>
-            </Grid>
+                  value={'accountLinking'}
+                  color="primary"
+                />
+              }
+              label={''}
+            />
+          </Box>
+          <Divider className={classes.divider} />
+          <Grid container spacing={2} className={classes.innerGrid}>
+            {settingsState.active && renderSettingsFields()}
           </Grid>
-        </Paper>
-      </Container>
-  )
-}
+          <Grid item container xs={12} justify={'flex-end'} style={{ marginTop: 16 }}>
+            <Button onClick={() => handleCancel()} style={{ marginRight: 16 }} color={'primary'}>
+              Cancel
+            </Button>
+            <Button variant="contained" color="primary" style={{ alignSelf: 'flex-end' }} onClick={() => save()}>
+              Save
+            </Button>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Container>
+  );
+};
 
 export default StorageSettings;
