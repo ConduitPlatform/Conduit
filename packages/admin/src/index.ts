fdadf2d6026909861be331582c4c1b91d16aeb99
Chris <capostolidis1@gmail.com>
2020-05-11 15:38:32 +0300

    fix: admin index removed testing code

--- packages/admin/src/index.ts
+++ packages/admin/src/index.ts
@@ -57,9 +57,8 @@ export default class AdminModule extends IConduitAdmin {
 
 
         const AdminModel = await databaseAdapter.getSchema('Admin');
-        databaseAdapter.findOne('Admin', JSON.stringify({username: 'admin'}))
+        databaseAdapter.findOne('Admin', {username: 'admin'})
             .then((existing: any) => {
-                existing = JSON.parse(existing);
                 if (isNil(existing)) {
                     const hashRounds = config.get('admin.auth.hashRounds');
                     return hashPassword('admin', hashRounds);
