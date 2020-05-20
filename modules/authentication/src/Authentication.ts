36b489b276f38bcc091244a2d4d7212cef03d03b
Chris <capostolidis1@gmail.com>
2020-05-20 15:42:50 +0300

    fix: removed AdminHandlers initialization from the constructor

--- modules/authentication/src/Authentication.ts
+++ modules/authentication/src/Authentication.ts
@@ -50,7 +50,6 @@ export default class AuthenticationModule {
     this.grpcServer.addService(authentication.service, {
       setConfig: this.setConfig.bind(this)
     });
-    this._admin = new AdminHandlers(this.grpcServer, grpcSdk);
     this._url = process.env.SERVICE_URL || '0.0.0.0:0';
     let result = this.grpcServer.bind(this._url, grpcModule.ServerCredentials.createInsecure());
     this._url = process.env.SERVICE_URL || ('0.0.0.0:' + result);
