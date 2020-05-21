c8ee13d5c44419aa5e37d5d24f5fb15e14267f5a
Chris <capostolidis1@gmail.com>
2020-05-21 11:50:52 +0300

    add: cms grpc admin methods for created schemas documents

--- modules/cms/src/admin/admin.ts
+++ modules/cms/src/admin/admin.ts
@@ -32,7 +32,12 @@ export class AdminHandlers {
       createSchema: this.createSchema.bind(this),
       toggle: this.toggle.bind(this),
       editSchema: this.editSchema.bind(this),
-      deleteSchema: this.deleteSchema.bind(this)
+      deleteSchema: this.deleteSchema.bind(this),
+      getDocuments: this.getDocuments.bind(this),
+      getDocumentById: this.getDocumentById.bind(this),
+      createDocument: this.createDocument.bind(this),
+      editDocument: this.editDocument.bind(this),
+      deleteDocument: this.deleteDocument.bind(this)
     });
 
   }
@@ -217,4 +222,139 @@ export class AdminHandlers {
     return callback(null, {result: 'Schema successfully deleted'});
   }
 
+  async getDocuments(call: any, callback: any) {
+    const { skip, limit, schemaName } = JSON.parse(call.request.params);
+
+    const schema = await this.database.findOne('SchemaDefinitions', {name: schemaName});
+    if (isNil(schema)) {
+      return callback({
+        code: grpc.status.NOT_FOUND,
+        message: 'Requested cms schema not found',
+      });
+    }
+
+    let skipNumber = 0, limitNumber = 25;
+
+    if (!isNil(skip)) {
+      skipNumber = Number.parseInt(skip as string);
+    }
+    if (!isNil(limit)) {
+      limitNumber = Number.parseInt(limit as string);
+    }
+
+    let errorMessage: any = null;
+    const documentsPromise = this.database.findMany(schemaName, {}, null, skipNumber, limitNumber);
+    const countPromise = this.database.countDocuments(schemaName, {});
+
+    const [documents, documentsCount] = await Promise.all([documentsPromise, countPromise]).catch((e: any) => errorMessage = e.message);
+    if (!isNil(errorMessage)) {
+      return callback({
+        code: grpc.status.INTERNAL,
+        message: errorMessage,
+      });
+    }
+
+    return callback(null, {result: JSON.stringify({documents, documentsCount})});
+  }
+
+  async getDocumentById(call: any, callback: any) {
+    const {schemaName, id} = JSON.parse(call.request.params);
+
+    const schema = await this.database.findOne('SchemaDefinitions', {name: schemaName});
+    if (isNil(schema)) {
+      return callback({
+        code: grpc.status.NOT_FOUND,
+        message: 'Requested cms schema not found',
+      });
+    }
+
+    let errorMessage: any = null;
+    const document = await this.database.findOne(schemaName, {_id: id}).catch((e: any) => errorMessage = e.message);
+    if (!isNil(errorMessage)) {
+      return callback({
+        code: grpc.status.INTERNAL,
+        message: errorMessage,
+      });
+    }
+    if (isNil(document)) {
+      return callback({
+        code: grpc.status.NOT_FOUND,
+        message: 'Requested document not found',
+      });
+    }
+    return callback(null, {result: JSON.stringify(document)});
+  }
+
+  async createDocument(call: any, callback: any) {
+    const {schemaName, inputDocument} = JSON.parse(call.request.params);
+
+    const schema = await this.database.findOne('SchemaDefinitions', {name: schemaName});
+    if (isNil(schema)) {
+      return callback({
+        code: grpc.status.NOT_FOUND,
+        message: 'Requested cms schema not found',
+      });
+    }
+
+    let errorMessage: any = null;
+    const newDocument = await this.database.create(schemaName, inputDocument).catch((e: any) => errorMessage = e.message);
+    if (!isNil(errorMessage)) {
+      return callback({
+        code: grpc.status.INTERNAL,
+        message: errorMessage,
+      });
+    }
+
+    return callback(null, {result: JSON.stringify(newDocument)});
+  }
+
+  async editDocument(call: any, callback: any) {
+    const {schemaName, id, changedDocument} = JSON.parse(call.request.params);
+
+    const schema = await this.database.findOne('SchemaDefinitions', {name: schemaName});
+    if (isNil(schema)) {
+      return callback({
+        code: grpc.status.NOT_FOUND,
+        message: 'Requested cms schema not found',
+      });
+    }
+
+    let errorMessage: any = null;
+    const dbDocument = await this.database.findOne(schemaName, {_id: id}).catch((e: any) => errorMessage = e.message);
+    if (!isNil(errorMessage)) {
+      return callback({
+        code: grpc.status.INTERNAL,
+        message: errorMessage,
+      });
+    }
+
+    Object.assign(dbDocument, changedDocument);
+
+    const updatedDocument = await this.database.findByIdAndUpdate(schemaName, dbDocument);
+
+    return callback(null, {result: JSON.stringify(updatedDocument)});
+  }
+
+  async deleteDocument(call: any, callback: any) {
+    const {schemaName, id} = JSON.parse(call.request.params);
+
+    const schema = await this.database.findOne('SchemaDefinitions', {name: schemaName});
+    if (isNil(schema)) {
+      return callback({
+        code: grpc.status.NOT_FOUND,
+        message: 'Requested cms schema not found',
+      });
+    }
+
+    let errorMessage: any = null;
+    await this.database.deleteOne(schemaName, {_id: id}).catch((e: any) => errorMessage = e.message);
+    if (!isNil(errorMessage)) {
+      return callback({
+        code: grpc.status.INTERNAL,
+        message: errorMessage,
+      });
+    }
+
+    return callback(null, {result: 'Ok'});
+  }
 }
