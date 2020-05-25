7192f0f888fdd22e2b1c50e5d1e8c1643bb26ba8
Chris <capostolidis1@gmail.com>
2020-05-25 17:17:32 +0300

    fix: wait until authentication routes are assigned before attempting to register them on grpcSdk router

--- modules/authentication/src/index.ts
+++ modules/authentication/src/index.ts
@@ -2,6 +2,7 @@ import ConduitGrpcSdk from "@conduit/grpc-sdk";
 import fs from "fs";
 import * as path from 'path';
 import AuthenticationModule from './Authentication';
+import { isNil } from "lodash";
 
 let paths = require("./admin/admin.json")
 
@@ -18,10 +19,23 @@ grpcSdk.admin.register(paths.functions, protofile.toString('UTF-8'),authenticati
   console.error(err);
 });
 let routerProtoFile = fs.readFileSync(path.resolve(__dirname, './routes/router.proto'));
-grpcSdk.router.register(authentication.routes, routerProtoFile.toString('UTF-8'), authentication.url).catch((err: Error) => {
-  console.log('Failed to register routes for authentication module');
-  console.log(err);
-});
+
+getAuthRoutes(authentication)
+  .then((authRoutes: any[]) => {
+    return grpcSdk.router.register(authRoutes, routerProtoFile.toString('UTF-8'), authentication.url);
+  })
+  .catch((err: Error) => {
+    console.log('Failed to register routes for authentication module');
+    console.log(err);
+  });
 // } else {
 //     throw new Error("Conduit server URL not provided");
 // }
+async function getAuthRoutes(authentication: any): Promise<any> {
+  let authRoutes = authentication.routes;
+  if (isNil(authRoutes)) {
+    await new Promise(r => setTimeout(r, 2000));
+    return getAuthRoutes(authentication);
+  }
+  return authRoutes;
+}
