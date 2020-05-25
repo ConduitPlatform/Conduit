c30fa2781cbf5bff89363dc71a44fcb08fd7c2f4
Chris <capostolidis1@gmail.com>
2020-05-25 13:33:50 +0300

    add: processes names on pm2 ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,11 @@
 module.exports = {
     apps: [{
+        name: 'Core',
         script: './packages/core/dist/bin/www.js',
         // watch: '.'
     },
         {
+            name: 'Database-provider',
             script: './modules/database-provider/dist/index.js',
             // watch: ['./service-worker']
         }],
