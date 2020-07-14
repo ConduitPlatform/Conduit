35f47e65618c738140754d8f8f8ea4b3f7669cd6
kkopanidis <kkopanidis@gmail.com>
2020-07-15 00:15:10 +0300

    add dockerfile for db provider and admin

--- modules/database-provider/src/index.ts
+++ modules/database-provider/src/index.ts
@@ -5,10 +5,18 @@ if (process.env.CONDUIT_SERVER) {
     let grpcSdk = new ConduitGrpcSdk(process.env.CONDUIT_SERVER);
     let databaseProvider = new DatabaseProvider(grpcSdk);
     databaseProvider.ensureIsRunning().then(() => {
-        grpcSdk.config.registerModule('database-provider', databaseProvider.url).catch(err => {
-            console.error(err)
-            process.exit(-1);
-        });
+        if(process.env.REGISTER_NAME){
+            grpcSdk.config.registerModule('database-provider', 'database-provider:'+databaseProvider.url.split(':')[1]).catch(err => {
+                console.error(err)
+                process.exit(-1);
+            });
+        }else{
+            grpcSdk.config.registerModule('database-provider', databaseProvider.url).catch(err => {
+                console.error(err)
+                process.exit(-1);
+            });
+        }
+        
     });
 } else {
     throw new Error("Conduit server URL not provided");
