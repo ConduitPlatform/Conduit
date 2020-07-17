4715f9f600236c4f7b4f795265dc759c1b7c98b3
kkopanidis <kkopanidis@gmail.com>
2020-07-17 16:53:17 +0300

    fix: publish to github packages

--- modules/database-provider/src/index.ts
+++ modules/database-provider/src/index.ts
@@ -5,17 +5,15 @@ if (process.env.CONDUIT_SERVER) {
     let grpcSdk = new ConduitGrpcSdk(process.env.CONDUIT_SERVER);
     let databaseProvider = new DatabaseProvider(grpcSdk);
     databaseProvider.ensureIsRunning().then(() => {
+        let url = databaseProvider.url;
         if(process.env.REGISTER_NAME){
-            grpcSdk.config.registerModule('database-provider', 'database-provider:'+databaseProvider.url.split(':')[1]).catch(err => {
-                console.error(err)
-                process.exit(-1);
-            });
-        }else{
-            grpcSdk.config.registerModule('database-provider', databaseProvider.url).catch(err => {
-                console.error(err)
-                process.exit(-1);
-            });
+            url = 'database-provider:'+url.split(':')[1];
         }
+        grpcSdk.config.registerModule('database-provider', url)
+        .catch((err: any) => {
+            console.error(err)
+            process.exit(-1);
+        });
         
     });
 } else {
--- modules/email/src/index.ts
+++ modules/email/src/index.ts
@@ -8,12 +8,17 @@ let paths = require("./admin/admin.json")
 if (process.env.CONDUIT_SERVER) {
     let grpcSdk = new ConduitGrpcSdk(process.env.CONDUIT_SERVER);
     let email = new EmailModule(grpcSdk);
-    grpcSdk.config.registerModule('email', email.url).catch((err: any) => {
+    let url = email.url;
+    if(process.env.REGISTER_NAME){
+        url = 'email-provider:'+url.split(':')[1];
+    
+    }
+    grpcSdk.config.registerModule('email', url).catch((err: any) => {
         console.error(err)
         process.exit(-1);
     });
     let protofile = fs.readFileSync(path.resolve(__dirname, './admin/admin.proto'))
-    grpcSdk.admin.register(paths.functions, protofile.toString('utf-8'), email.url).catch((err: Error) => {
+    grpcSdk.admin.register(paths.functions, protofile.toString('utf-8'), url).catch((err: Error) => {
         console.log("Failed to register admin routes for email module!")
         console.error(err);
     });
