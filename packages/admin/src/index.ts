0e64c70b6d5da3df099e42f292ce4f09214fb575
kkopanidis <kkopanidis@gmail.com>
2020-09-14 18:43:57 +0300

    fix: admin requests with string result crashing the server

--- packages/admin/src/index.ts
+++ packages/admin/src/index.ts
@@ -123,7 +123,13 @@ export default class AdminModule extends IConduitAdmin {
                     if (err) {
                         return res.status(500).send(err);
                     }
-                    res.status(200).json(JSON.parse(result.result));
+                    let sendResult = result.result;
+                    try {
+                        sendResult = JSON.parse(result.result)
+                    } catch (error) {
+                        // do nothing
+                    }
+                    res.status(200).json(sendResult);
                 });
             }
             this.registerRoute(r.method, r.path, handler)
