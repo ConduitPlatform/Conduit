f97d1433f1160438194c2af66e85010658cd5787
JohnChantz <g.chantzigoulas@quintessential.gr>
2020-12-01 15:30:12 +0200

    Update SelectForm.js

--- admin/app/src/components/cms/types/SelectType/SelectForm.js
+++ admin/app/src/components/cms/types/SelectType/SelectForm.js
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import slugify from '../../../../utils/slugify';
 
 const useStyles = makeStyles((theme) => ({
@@ -42,13 +42,9 @@ export default function SelectForm({
     type: selectedItem ? selectedItem.type : drawerData.type,
   });
 
-  useEffect(() => {
-    const slug = slugify(selectData.name);
-    setSelectData({ ...selectData, id: slug });
-  }, [selectData, selectData.name]);
-
   const handleFieldName = (event) => {
-    setSelectData({ ...selectData, name: event.target.value });
+    const slug = slugify(event.target.value);
+    setSelectData({ ...selectData, name: event.target.value, id: slug });
   };
 
   const handleApiId = (event) => {
