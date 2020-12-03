f784a579e426fd2269ce25b3cc24225a0d362485
kkopanidis <kkopanidis@gmail.com>
2020-12-03 17:31:40 +0200

    fix: docker-build.sh

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,3 +1,3 @@
-docker build -t conduit-base:latest -f ../Dockerfile ../
-
-docker build -t quintessential.azurecr.io/conduit:latest -f ../packages/Dockerfile ../
+docker build -t conduit-base:latest -f ../Dockerfile ../;
+docker build -t quintessential.azurecr.io/conduit:latest -f ../packages/Dockerfile ../ && docker push quintessential.azurecr.io/conduit:latest;
+lerna run build:docker
