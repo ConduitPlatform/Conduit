344076472ddba4afe0e4751ed7e7c95054621dad
kkopanidis <kkopanidis@gmail.com>
2020-12-06 21:40:41 +0200

    fix: dockerfiles reducing build sizes by several gigabytes

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,3 +1,3 @@
-docker build -t conduit-base:latest -f ../Dockerfile ../;
-docker build -t quintessential.azurecr.io/conduit:latest -f ../packages/Dockerfile ../ && docker push quintessential.azurecr.io/conduit:latest;
+docker build -t conduit-base:latest -f ./Dockerfile ./;
+docker build -t quintessential.azurecr.io/conduit:latest -f ./packages/Dockerfile ./ && docker push quintessential.azurecr.io/conduit:latest;
 yarn lerna run build:docker
