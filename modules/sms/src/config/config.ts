35d940658e4a822c0dd7c7f7cc9959c487835a81
NikosSiak <nikos.siakas@gmail.com>
2020-12-16 16:34:38 +0200

    sms config

---
+++ modules/sms/src/config/config.ts
@@ -0,0 +1,25 @@
+export default {
+    doc: 'The options for the conduit sms provider',
+    active: {
+        format: 'Boolean',
+        default: false
+    },
+    providerName: {
+        format: 'String',
+        default: 'twilio'
+    },
+    twilio: {
+        phoneNumber: {
+            format: 'String',
+            default: undefined
+        },
+        accountSID: {
+            format: 'String',
+            default: undefined
+        },
+        authToken: {
+            format: 'String',
+            default: undefined
+        }
+    }
+}
