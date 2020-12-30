32548dfbb0a9ccd192ee4e3c950081b116036247
kkopanidis <kkopanidis@gmail.com>
2020-12-30 12:39:58 +0200

    fix: authentication config get

--- packages/config/src/admin/admin.ts
+++ packages/config/src/admin/admin.ts
@@ -1,7 +1,7 @@
-import ConduitGrpcSdk from '@quintessential-sft/conduit-grpc-sdk';
-import { ConduitSDK } from '@quintessential-sft/conduit-sdk';
-import { Request, Response } from 'express';
-import { isNil, isEmpty } from 'lodash';
+import ConduitGrpcSdk from "@quintessential-sft/conduit-grpc-sdk";
+import { ConduitSDK } from "@quintessential-sft/conduit-sdk";
+import { Request, Response } from "express";
+import { isNil, isEmpty } from "lodash";
 
 export class AdminHandlers {
   private readonly database: any;
@@ -17,20 +17,19 @@ export class AdminHandlers {
       registeredModules.forEach((value: string, key: string) => {
         modules.push({
           moduleName: key,
-          url: value
-        })
+          url: value,
+        });
       });
       return res.json({ modules });
     } else {
-      return res.status(404).json({ message: 'Modules not available' });
+      return res.status(404).json({ message: "Modules not available" });
     }
   }
 
   async getConfig(req: Request, res: Response) {
-
     const registeredModules: Map<string, string> = (req as any).conduit.registeredModules;
 
-    const dbConfig = await this.database.findOne('Config', {});
+    const dbConfig = await this.database.findOne("Config", {});
     if (isNil(dbConfig)) {
       return res.json({});
     }
@@ -46,40 +45,40 @@ export class AdminHandlers {
         delete finalConfig.updatedAt;
         delete finalConfig.__v;
         break;
-      case 'authentication':
-        if (!registeredModules.has(module)) return res.json({ message: 'Module not available' });
+      case "authentication":
+        if (!registeredModules.has(module)) return res.json({ message: "Module not available" });
         finalConfig = dbConfig.moduleConfigs.authentication;
         break;
-      case 'email':
-        if (!registeredModules.has(module)) return res.json({ message: 'Module not available' });
+      case "email":
+        if (!registeredModules.has(module)) return res.json({ message: "Module not available" });
         finalConfig = dbConfig.moduleConfigs.email;
         break;
-      case 'storage':
-        if (!registeredModules.has(module)) return res.json({ message: 'Module not available' });
+      case "storage":
+        if (!registeredModules.has(module)) return res.json({ message: "Module not available" });
         finalConfig = dbConfig.moduleConfigs.storage;
         break;
-      case 'push-notifications':
-        if (!registeredModules.has(module)) return res.json({ message: 'Module not available' });
+      case "push-notifications":
+        if (!registeredModules.has(module)) return res.json({ message: "Module not available" });
         finalConfig = dbConfig.moduleConfigs.pushNotifications;
         break;
-      case 'in-memory-store':
-        if (!registeredModules.has(module)) return res.json({ message: 'Module not available' });
+      case "in-memory-store":
+        if (!registeredModules.has(module)) return res.json({ message: "Module not available" });
         finalConfig = dbConfig.moduleConfigs.inMemoryStore;
         break;
       default:
-        return res.status(404).json({ error: 'Resource not found' });
+        return res.status(404).json({ error: "Resource not found" });
     }
 
-    if (isEmpty(finalConfig)) return res.json({active: false});
+    if (isEmpty(finalConfig)) return res.json({ active: false });
     return res.json(finalConfig);
   }
 
   async setConfig(req: Request, res: Response) {
     const registeredModules: Map<string, string> = (req as any).conduit.registeredModules;
 
-    const dbConfig = await this.database.findOne('Config', {});
+    const dbConfig = await this.database.findOne("Config", {});
     if (isNil(dbConfig)) {
-      return res.status(404).json({ error: 'Config not set' });
+      return res.status(404).json({ error: "Config not set" });
     }
 
     const newConfig = req.body;
@@ -87,8 +86,8 @@ export class AdminHandlers {
     let errorMessage: string | null = null;
     let updatedConfig: any;
 
-    if (newConfig.active === false) return res.status(403).json({error: 'Modules cannot be deactivated'});
-
+    if (newConfig.active === false) return res.status(403).json({ error: "Modules cannot be deactivated" });
+    await this.grpcSdk.initializeModules().catch((err) => console.log("Failed to refresh modules"));
     switch (moduleName) {
       case undefined:
         // TODO changing module settings through this endpoint completely bypasses the running check and is not secure
@@ -96,34 +95,50 @@ export class AdminHandlers {
         //   errorMessage = 'Invalid configuration fields';
         //   break;
         // }
-        updatedConfig = await this.sdk.updateConfig(newConfig).catch((e: Error) => errorMessage = e.message);
+        updatedConfig = await this.sdk.updateConfig(newConfig).catch((e: Error) => (errorMessage = e.message));
         break;
-      case 'authentication':
-        if (!registeredModules.has(moduleName) || isNil(this.grpcSdk.authentication)) return res.json({ message: 'Module not available' });
-        updatedConfig = await this.sdk.getAuthentication().setConfig(newConfig).catch((e: Error) => errorMessage = e.message);
+      case "authentication":
+        if (!registeredModules.has(moduleName) || isNil(this.grpcSdk.authentication))
+          return res.json({ message: "Module not available" });
+        updatedConfig = await this.grpcSdk.authentication
+          .setConfig(newConfig)
+          .catch((e: Error) => (errorMessage = e.message));
         break;
-      case 'email':
-        if (!registeredModules.has(moduleName) || isNil(this.grpcSdk.emailProvider)) return res.json({ message: 'Module not available' });
-        updatedConfig = await this.grpcSdk.emailProvider.setConfig(newConfig).catch((e: Error) => errorMessage = e.message);
+      case "email":
+        if (!registeredModules.has(moduleName) || isNil(this.grpcSdk.emailProvider))
+          return res.json({ message: "Module not available" });
+        updatedConfig = await this.grpcSdk.emailProvider
+          .setConfig(newConfig)
+          .catch((e: Error) => (errorMessage = e.message));
         break;
-      case 'in-memory-store':
-        if (!registeredModules.has(moduleName) || isNil(this.grpcSdk.inMemoryStore)) return res.json({ message: 'Module not available' });
-        updatedConfig = await this.grpcSdk.inMemoryStore.setConfig(newConfig).catch((e: Error) => errorMessage = e.message);
+      case "in-memory-store":
+        if (!registeredModules.has(moduleName) || isNil(this.grpcSdk.inMemoryStore))
+          return res.json({ message: "Module not available" });
+        updatedConfig = await this.grpcSdk.inMemoryStore
+          .setConfig(newConfig)
+          .catch((e: Error) => (errorMessage = e.message));
         break;
-      case 'push-notifications':
-        if (!registeredModules.has(moduleName) || isNil(this.grpcSdk.pushNotifications)) return res.json({ message: 'Module not available' });
-        updatedConfig = await this.sdk.getPushNotifications().setConfig(newConfig).catch((e: Error) => errorMessage = e.message);
+      case "push-notifications":
+        if (!registeredModules.has(moduleName) || isNil(this.grpcSdk.pushNotifications))
+          return res.json({ message: "Module not available" });
+        updatedConfig = await this.sdk
+          .getPushNotifications()
+          .setConfig(newConfig)
+          .catch((e: Error) => (errorMessage = e.message));
         break;
-      case 'storage':
-        if (!registeredModules.has(moduleName) || isNil(this.grpcSdk.storage)) return res.json({ message: 'Module not available' });
-        updatedConfig = await this.sdk.getStorage().setConfig(newConfig).catch((e: Error) => errorMessage = e.message);
+      case "storage":
+        if (!registeredModules.has(moduleName) || isNil(this.grpcSdk.storage))
+          return res.json({ message: "Module not available" });
+        updatedConfig = await this.sdk
+          .getStorage()
+          .setConfig(newConfig)
+          .catch((e: Error) => (errorMessage = e.message));
         break;
       default:
-        return res.status(404).json({ error: 'Resource not found' });
+        return res.status(404).json({ error: "Resource not found" });
     }
 
-    if (!isNil(errorMessage)) return res.status(403).json({error: errorMessage});
+    if (!isNil(errorMessage)) return res.status(403).json({ error: errorMessage });
     return res.json(updatedConfig);
-
   }
 }
