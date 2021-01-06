eeb9a900cede7e4165221ff93b9d7f019c5228c1
kkopanidis <kkopanidis@gmail.com>
2021-01-06 13:16:42 +0200

    refine: docker build process to save build times

---
+++ scripts/Dockerfile.builder
@@ -0,0 +1,9 @@
+FROM node:dubnium-alpine
+
+WORKDIR /app
+
+COPY --from=conduit-base:latest /app/libraries /app/libraries
+COPY --from=conduit-base:latest /app/package.json .
+COPY --from=conduit-base:latest /app/yarn.lock .
+
+RUN apk add --no-cache --virtual .gyp python make g++
--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,3 +1,4 @@
-docker build -t conduit-base:latest -f ./Dockerfile ./;
-docker build -t quintessential.azurecr.io/conduit:latest -f ./packages/Dockerfile ./ && docker push quintessential.azurecr.io/conduit:latest;
-npx lerna run build:docker
+time docker build -t conduit-base:latest -f ./Dockerfile ./;
+time docker build -t conduit-builder:latest -f ./scripts/Dockerfile.builder ./scripts;
+time docker build -t quintessential.azurecr.io/conduit:latest -f ./packages/Dockerfile ./ && docker push quintessential.azurecr.io/conduit:latest;
+time npx lerna run build:docker
