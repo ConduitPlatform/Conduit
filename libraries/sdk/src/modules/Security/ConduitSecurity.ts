0f21c7d0a5faab36887552016135e0330d80ce02
kkopanidis <kkopanidis@gmail.com>
2021-01-14 17:40:43 +0200

    refine(security)

--- libraries/sdk/src/modules/Security/ConduitSecurity.ts
+++ libraries/sdk/src/modules/Security/ConduitSecurity.ts
@@ -1,10 +1,7 @@
 import { ConduitSDK } from '../../index';
-import { NextFunction, Request, Response } from 'express';
 
 export abstract class IConduitSecurity {
 
   constructor(conduit: ConduitSDK) {
   }
-
-  abstract clientMiddleware(req: Request, res: Response, next: NextFunction): void;
 }
--- packages/security/src/index.ts
+++ packages/security/src/index.ts
@@ -1,61 +1,24 @@
-import {NextFunction, Request, Response} from 'express';
-import {ClientModel} from './models/Client';
-import {isNil} from 'lodash';
-import {ConduitSDK, IConduitSecurity, ConduitError} from '@quintessential-sft/conduit-sdk';
+import {ConduitSDK, IConduitSecurity} from '@quintessential-sft/conduit-sdk';
 import ConduitGrpcSdk from '@quintessential-sft/conduit-grpc-sdk';
 import {Admin} from './admin';
 import helmet from "helmet";
 import {RateLimiter} from "./handlers/rate-limiter";
+import {ClientValidator} from "./handlers/client-validation";
 
 class SecurityModule extends IConduitSecurity {
 
-    private readonly database: any;
-    conduit: ConduitSDK;
-
-    constructor(conduit: ConduitSDK, grpcSdk: ConduitGrpcSdk) {
+    constructor(private readonly conduit: ConduitSDK, private readonly grpcSdk: ConduitGrpcSdk) {
         super(conduit);
 
-        this.conduit = conduit;
-        this.database = grpcSdk.databaseProvider!;
-        this.database.createSchemaFromAdapter(ClientModel);
-
-        new Admin(this.conduit, grpcSdk);
-
+        new Admin(conduit, grpcSdk);
         const router = conduit.getRouter();
+
+        let clientValidator: ClientValidator = new ClientValidator(grpcSdk.databaseProvider!);
 
         router.registerGlobalMiddleware('rateLimiter', (new RateLimiter(process.env.REDIS_HOST as string,
             parseInt(process.env.REDIS_PORT as string))).limiter);
-        router.registerGlobalMiddleware('clientMiddleware', this.clientMiddleware.bind(this));
         router.registerGlobalMiddleware('helmetMiddleware', helmet());
-
-    }
-
-    clientMiddleware(req: Request, res: Response, next: NextFunction) {
-        if (isNil((req as any).conduit)) (req as any).conduit = {};
-        if (req.path.indexOf('/hook') === 0 || req.path.indexOf('/admin') === 0) {
-            return next();
-        }
-
-        if ((req.url === '/graphql' || req.url.startsWith('/swagger')) && req.method === 'GET') {
-            return next();
-        }
-
-        const {clientid, clientsecret} = req.headers;
-        if (isNil(clientid) || isNil(clientsecret)) {
-            return next(ConduitError.unauthorized());
-        }
-
-        this.database
-            .findOne('Client', {clientId: clientid, clientSecret: clientsecret})
-            .then((client: any) => {
-                if (isNil(client)) {
-                    throw ConduitError.unauthorized();
-                }
-                delete req.headers.clientsecret;
-                (req as any).conduit.clientId = clientid;
-                next();
-            })
-            .catch(next);
+        router.registerGlobalMiddleware('clientMiddleware', clientValidator.middleware.bind(clientValidator));
     }
 
 }
