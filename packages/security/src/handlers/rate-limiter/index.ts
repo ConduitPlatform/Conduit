869be64f5d48fe72b411e10c6b803e3135b68629
kkopanidis <kkopanidis@gmail.com>
2021-01-14 17:27:14 +0200

    add(security): rate limiting add(sdk): redis package and types

---
+++ packages/security/src/handlers/rate-limiter/index.ts
@@ -0,0 +1,40 @@
+import {RedisClient} from "redis";
+import {RateLimiterRedis} from "rate-limiter-flexible";
+import {NextFunction} from "express";
+
+export class RateLimiter {
+
+    private _limiter: any;
+
+    constructor(redisHost: string, redisPort: number) {
+        let redisClient: RedisClient = new RedisClient({
+            host: redisHost,
+            port: redisPort,
+            enable_offline_queue: false,
+        });
+        this._limiter = new RateLimiterRedis({
+            storeClient: redisClient,
+            keyPrefix: 'mainLimiter',
+            points: 50, // 10 requests
+            duration: 1, // per 1 second by IP
+            blockDuration: 10,
+            inmemoryBlockOnConsumed: 10,
+            execEvenly: false
+        });
+
+    }
+
+    get limiter() {
+        const self = this;
+        return (req: any, res: any, next: any) => {
+            self._limiter.consume(req.ip)
+                .then(() => {
+                    next();
+                })
+                .catch(() => {
+                    res.status(429).send('Too Many Requests');
+                });
+        };
+    }
+
+}
