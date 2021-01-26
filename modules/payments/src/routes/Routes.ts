14beb16f1ba4ccd82bd228e72a82064814d4ff99
NikosSiak <nikos.siakas@gmail.com>
2021-01-26 14:10:09 +0200

    add get products to payments routes

--- modules/payments/src/routes/Routes.ts
+++ modules/payments/src/routes/Routes.ts
@@ -4,15 +4,27 @@ import ConduitGrpcSdk, {
   ConduitRouteActions,
   ConduitRouteReturnDefinition,
   constructRoute,
+  TYPE,
+  ConduitString
 } from "@quintessential-sft/conduit-grpc-sdk";
 import fs from "fs";
 import path from "path";
+import { isNil } from "lodash";
 
 const protoLoader = require("@grpc/proto-loader");
 const PROTO_PATH = __dirname + "/router.proto";
 
 export class PaymentsRoutes {
+  private database: any;
+
   constructor(server: grpc.Server, private readonly grpcSdk: ConduitGrpcSdk) {
+    const self = this;
+
+    grpcSdk.waitForExistence('database-provider')
+      .then(r => {
+        self.database = self.grpcSdk.databaseProvider;
+      });
+
     const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
       keepCase: true,
       longs: String,
@@ -25,8 +37,25 @@ export class PaymentsRoutes {
     // @ts-ignore
     const router = protoDescriptor.payments.router.Router;
     server.addService(router.service, {
+      getProducts: this.getProducts.bind(this),
       completePayment: this.completePayment.bind(this),
     });
+  }
+
+  async getProducts(call: any, callback: any) {
+    let errorMessage: string | null = null;
+    const products = await this.database.findMany('Product', {})
+      .catch((e: Error) => {
+        errorMessage = e.message;
+      });
+    if (!isNil(errorMessage)) {
+      return callback({
+        code: grpc.status.INTERNAL,
+        message: errorMessage
+      });
+    }
+
+    return callback(null, { result: JSON.stringify({ products }) });
   }
 
   completePayment(call: any, callback: any) {
@@ -44,6 +73,28 @@ export class PaymentsRoutes {
 
   getRegisteredRoutes(): any[] {
     let routesArray: any[] = [];
+
+    routesArray.push(
+      constructRoute(
+        new ConduitRoute(
+          {
+            path: "/hook/payments/products",
+            action: ConduitRouteActions.GET,
+          },
+          new ConduitRouteReturnDefinition('GetProductsResponse', {
+            products: [{
+              _id: TYPE.String,
+              name: TYPE.String,
+              value: TYPE.Number,
+              currency: TYPE.String,
+              isSubscription: TYPE.Boolean,
+              renewEvery: ConduitString.Optional
+            }]
+          }),
+          "getProducts"
+        )
+      )
+    );
 
     routesArray.push(
       constructRoute(
