433d8be9e7a9869bd68ec505817dc6f3f43ca051
NikosSiak <nikos.siakas@gmail.com>
2021-03-12 12:21:11 +0200

    database: remove try catch in create with population

--- modules/database/src/adapters/mongoose-adapter/utils.ts
+++ modules/database/src/adapters/mongoose-adapter/utils.ts
@@ -2,6 +2,9 @@ import { ConduitModel } from '@quintessential-sft/conduit-grpc-sdk';
 import { MongooseAdapter } from './index';
 import { isArray, isObject } from 'lodash';
 
+/**
+ * @throws {Error}
+ */
 async function _createWithPopulations(
   fields: ConduitModel,
   document: { [key: string]: any },
@@ -12,50 +15,49 @@ async function _createWithPopulations(
     if (!isObject(fields[key])) continue;
     if (!fields.hasOwnProperty(key)) continue;
 
-    try {
-      if (isArray(document[key])) {
-        for (let i = 0; i < document[key].length; i++) {
-          let val = document[key][i];
-          if (!isObject(val)) {
-            continue;
-          }
-          // @ts-ignore
-          if (fields[key][0].hasOwnProperty('ref')) {
-            // @ts-ignore
-            const model = adapter.getSchemaModel(fields[key][0].ref);
-            if (validate) {
-              const d = new model.model(val);
-              console.log(d);
-            } else {
-              const doc = await model.create(val);
-              document[key][i] = doc._id;
-            }
-          } else {
-            // @ts-ignore
-            await _createWithPopulations(fields[key][0], val, adapter, validate);
-          }
+    if (isArray(document[key])) {
+      for (let i = 0; i < document[key].length; i++) {
+        let val = document[key][i];
+        if (!isObject(val)) {
+          continue;
         }
-      } else if (isObject(document[key])) {
-        if (fields[key].hasOwnProperty('ref')) {
+        // @ts-ignore
+        if (fields[key][0].hasOwnProperty('ref')) {
           // @ts-ignore
-          const model = adapter.getSchemaModel(fields[key].ref);
+          const model = adapter.getSchemaModel(fields[key][0].ref);
           if (validate) {
-            await model.model.validate(document[key]);
+            const d = new model.model(val);
+            console.log(d);
           } else {
-            const doc = await model.create(document[key]);
-            document[key] = doc._id;
+            const doc = await model.create(val);
+            document[key][i] = doc._id;
           }
         } else {
           // @ts-ignore
-          await _createWithPopulations(fields[key], document[key], adapter, validate);
+          await _createWithPopulations(fields[key][0], val, adapter, validate);
         }
       }
-    } catch (e) {
-      throw new Error(e.message);
+    } else if (isObject(document[key])) {
+      if (fields[key].hasOwnProperty('ref')) {
+        // @ts-ignore
+        const model = adapter.getSchemaModel(fields[key].ref);
+        if (validate) {
+          await model.model.validate(document[key]);
+        } else {
+          const doc = await model.create(document[key]);
+          document[key] = doc._id;
+        }
+      } else {
+        // @ts-ignore
+        await _createWithPopulations(fields[key], document[key], adapter, validate);
+      }
     }
   }
 }
 
+/**
+ * @throws {Error}
+ */
 export async function createWithPopulations(
   fields: ConduitModel,
   document: { [key: string]: any },
