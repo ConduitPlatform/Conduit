c8badc2c2fff98bb5d6c4e4a5aa00d25798016ad
jasonkatsman <it21634@hua.gr>
2021-04-06 17:43:25 +0300

    token expiration + devtools on comments

--- admin/app/src/redux/store.js
+++ admin/app/src/redux/store.js
@@ -4,6 +4,19 @@ import thunk from 'redux-thunk';
 
 let store;
 
+// export const initStore = (initialState = {}) => {
+//   const composeEnhancers =
+//     (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+//     compose;
+//
+//   store = createStore(
+//     rootReducer,
+//     initialState,
+//     composeEnhancers(applyMiddleware(thunk))
+//   );
+//   return store;
+// };
+
 export const initStore = (initialState = {}) => {
   store = createStore(rootReducer, initialState, applyMiddleware(thunk));
   return store;
