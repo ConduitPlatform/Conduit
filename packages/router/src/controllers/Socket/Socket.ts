b833e968e9afa47fd370717870b1ffc21d2f235f
NikosSiak <nikos.siakas@gmail.com>
2021-04-07 18:27:02 +0300

    add register conduit socket method

--- packages/router/src/controllers/Socket/Socket.ts
+++ packages/router/src/controllers/Socket/Socket.ts
@@ -1,30 +1,43 @@
 import { Application } from 'express';
 import { createServer, Server as httpServer } from 'http';
 import { Server as IOServer, ServerOptions, Socket } from 'socket.io';
+import { ConduitRouteReturnDefinition, ConduitSocket } from '@quintessential-sft/conduit-sdk';
 
 
 export class SocketController {
   private readonly httpServer: httpServer;
   private io: IOServer;
   private readonly options: Partial<ServerOptions>;
+  private _registeredNamespaces: Map<string, ConduitSocket>;
 
   constructor(private readonly app: Application) {
     this.httpServer = createServer(app);
     this.options = {};
     this.io = new IOServer(this.httpServer, this.options);
     this.httpServer.listen(process.env.SOCKET_PORT || 3001);
+    this._registeredNamespaces = new Map();
+  }
 
-    this.io.on('connect', (socket: Socket) => {
-      console.log(`connect ${socket.id}`);
+  registerConduitSocket(conduitSocket: ConduitSocket) {
+    const namespace = conduitSocket.input.path;
+    this._registeredNamespaces.set(namespace, conduitSocket);
+    this.io.of(namespace).on('connect', socket => {
 
-      socket.on("ping", (cb) => {
-        console.log("ping");
-        cb();
+      socket.onAny((event, ...args) => {
+
+        conduitSocket.executeRequest({
+          event,
+          params: args
+        })
+        .then(() => {
+
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+
       });
 
-      socket.on("disconnect", () => {
-        console.log(`disconnect ${socket.id}`);
-      });
-    })
+    });
   }
 }
