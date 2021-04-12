2d92f2990bcc4757b6c4eaeef6ea1af1d29bbeb8
NikosSiak <nikos.siakas@gmail.com>
2021-04-12 12:26:41 +0300

    add types for grpc socket request and response

--- libraries/grpc-sdk/src/types/index.ts
+++ libraries/grpc-sdk/src/types/index.ts
@@ -22,3 +22,21 @@ export type RouterResponse = GrpcResponse<Response | ResponseWithResult | Respon
 
 export type SetConfigRequest = GrpcRequest<{ newConfig: string }>;
 export type SetConfigResponse = GrpcResponse<{ updatedConfig: string }>;
+
+export type SocketRequest = GrpcRequest<{
+  event: string,
+  socketId: string,
+  params: string
+}>;
+
+type EventResponse = {
+  event: string,
+  data: string,
+  receivers?: string[]
+};
+
+type JoinRoomResponse = {
+  rooms: string[]
+}
+
+export type SocketResponse = GrpcResponse<EventResponse | JoinRoomResponse>;
