a2634b92b8f2502de6a564c48e1c41b148f30643
kkopanidis <kkopanidis@gmail.com>
2021-05-05 00:28:07 +0300

    feat(actor): data injection works
    Definitely not the craziest thing I've done in this project

--- modules/actor/src/controllers/flowCreator.ts
+++ modules/actor/src/controllers/flowCreator.ts
@@ -8,6 +8,27 @@ import { Webhook } from '../_triggers/webhook/webhook';
 
 const PROCESSOR_TEMPLATE = `
 {{flow_requirements}}
+
+const deepdash = require('deepdash/standalone');
+const _ = require('lodash');
+
+function injectData(injection, actorOptions){
+  let replacements = [];
+  
+  deepdash.eachDeep(actorOptions, (value, key, parent, context) => {
+    console.log("Iterating on key: "+key+" and value: "+value);
+    if (value && typeof value === 'string' && value.indexOf("inject:data") !== -1) {
+        replacements.push(context.path);      
+    }
+  });
+  replacements.forEach(replacement=>{
+    if(_.has(injection, actorOptions[replacement].replace("inject:", ""))){
+      actorOptions[replacement] = _.get(injection, actorOptions[replacement].replace("inject:", ""))
+    }
+  });
+  
+}
+
 module.exports = async function(job){
   let queueData = {
     data: {trigger: job.data},
@@ -20,7 +41,7 @@ module.exports = async function(job){
   try {
     {{flow_code}}
   } catch(e){
-    console.error(\"Actor " + executingActor + " crashed\");
+    console.error(\"Actor " + executingActor + " crashed with: \" + e);
     throw e;
   }
   return Promise.resolve(queueData);
@@ -61,17 +82,18 @@ export class FlowCreator {
       let actorAlias = actor.name ?? actor.code;
 
       flowCode += `
-      if(nextActor === null || nextActor === "${actor.code}"){
+      if(nextActor === null || nextActor === "${actorAlias}"){
+        executingActor = "${actorAlias}";
         nextActor = null;
         let ${actorAlias}Input = {
-          actorOptions: ${JSON.stringify(actor.options)},
+          actorOptions:  ${JSON.stringify(actor.options)},
           context: {...queueData}
         }
-        executingActor = ${actorAlias};
-        let ${actorAlias}Return = await ${actor.code}(${actor.code}Input);
-        if ( ${actorAlias}Return.goTo ){
-          nextActor = ${actorAlias}Return.gotTo;
-        }else {      
+        injectData( {data: queueData.data}, ${actorAlias}Input.actorOptions) 
+        let ${actorAlias}Return = await ${actor.code}(${actorAlias}Input);
+        if ( ${actorAlias}Return && ${actorAlias}Return.goTo ){
+          nextActor = ${actorAlias}Return.goTo;
+        } else if ( ${actorAlias}Return ) {      
           queueData.data[\"${actorAlias}\"] = ${actorAlias}Return.data
         }
         console.log(${actorAlias}Return)
