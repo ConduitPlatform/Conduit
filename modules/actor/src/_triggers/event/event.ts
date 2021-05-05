b89e947df9b1cd79d078f37b3ce84348e1e60740
kkopanidis <kkopanidis@gmail.com>
2021-05-05 16:42:57 +0300

    feat(actor): support for event triggers

---
+++ modules/actor/src/_triggers/event/event.ts
@@ -0,0 +1,42 @@
+import { Trigger } from '../../models/trigger.interface';
+import { EventInterface } from './event.interface';
+import ConduitGrpcSdk from '@quintessential-sft/conduit-grpc-sdk';
+
+export class Event implements Trigger<EventInterface> {
+
+  private static _instance: Event;
+  private busInitialized: boolean = false;
+
+  private constructor(private readonly grpcSdk: ConduitGrpcSdk) {
+    this.grpcSdk.initializeEventBus()
+      .then(() => {
+        console.log('Event bus initialized');
+        this.busInitialized = true;
+      })
+      .catch(err => {
+        console.error('Failed to initialize event trigger');
+        console.error(err);
+      });
+
+  }
+
+  public static getInstance(grpcSdk: ConduitGrpcSdk) {
+    if (!Event._instance) {
+      Event._instance = new Event(grpcSdk);
+    }
+    return Event._instance;
+  }
+
+  async setup(options: EventInterface): Promise<boolean> {
+    if (this.busInitialized) {
+      this.grpcSdk.bus!.subscribe(options.eventName, (data) => {
+        options.queue.add({ eventName: options.eventName, data: data });
+      });
+    } else {
+      throw new Error('Event bus is not initialized');
+    }
+    return Promise.resolve(true);
+  }
+
+
+}
