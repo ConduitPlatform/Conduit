8e6da16b45d886ff16a5b5554360b6399fc50804
dasno <ep.giannakopoulos@gmail.com>
2021-09-20 13:48:08 +0300

    add: CustomEndpoints / Email requests types

--- admin/app/src/http/CustomEndpointsRequests.ts
+++ admin/app/src/http/CustomEndpointsRequests.ts
@@ -4,12 +4,12 @@ import { CONDUIT_API } from './requestsConfig';
 export const getCustomEndpointsRequest = () => {
   return axios.get(`${CONDUIT_API}/admin/cms/customEndpoints`);
 };
-export const editCustomEndpointsRequest = (_id, endpointData) => {
+export const editCustomEndpointsRequest = (_id: string, endpointData: any) => {
   return axios.put(`${CONDUIT_API}/admin/cms/customEndpoints/${_id}`, endpointData);
 };
-export const deleteCustomEndpointsRequest = (_id) => {
+export const deleteCustomEndpointsRequest = (_id: string) => {
   return axios.delete(`${CONDUIT_API}/admin/cms/customEndpoints/${_id}`);
 };
-export const createCustomEndpointsRequest = (endpointData) => {
+export const createCustomEndpointsRequest = (endpointData: any) => {
   return axios.post(`${CONDUIT_API}/admin/cms/customEndpoints`, endpointData);
 };
