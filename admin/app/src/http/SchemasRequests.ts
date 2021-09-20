ee1e282e711e046fd31b83dc48e9e0ad17e4454c
dasno <ep.giannakopoulos@gmail.com>
2021-09-20 14:07:43 +0300

    add: Settings / Schemas requests types

--- admin/app/src/http/SchemasRequests.ts
+++ admin/app/src/http/SchemasRequests.ts
@@ -1,13 +1,17 @@
 import axios from 'axios';
 import { CONDUIT_API } from './requestsConfig';
 
-export const createSchemaDocumentRequest = (schemaName, documentData) =>
+export const createSchemaDocumentRequest = (schemaName: string, documentData: any) =>
   axios.post(`${CONDUIT_API}/admin/cms/content/${schemaName}`, { ...documentData });
 
-export const deleteSchemaDocumentRequest = (schemaName, documentId) =>
+export const deleteSchemaDocumentRequest = (schemaName: string, documentId: string) =>
   axios.delete(`${CONDUIT_API}/admin/cms/schemas/${schemaName}/${documentId}`);
 
-export const editSchemaDocumentRequest = (schemaName, documentId, documentData) =>
+export const editSchemaDocumentRequest = (
+  schemaName: string,
+  documentId: string,
+  documentData: any
+) =>
   axios.put(`${CONDUIT_API}/admin/cms/schemas/${schemaName}/${documentId}`, {
     ...documentData,
   });
--- admin/app/src/http/SettingsRequests.ts
+++ admin/app/src/http/SettingsRequests.ts
@@ -1,19 +1,20 @@
 import axios from 'axios';
 import { CONDUIT_API } from './requestsConfig';
+import { IPlatformTypes } from '../models/settings/SettingsModels';
 
 export const getAvailableClientsRequest = () => {
   return axios.get(`${CONDUIT_API}/admin/security/client`);
 };
 
-export const generateNewClientRequest = (platform) => {
+export const generateNewClientRequest = (platform: IPlatformTypes) => {
   return axios.post(`${CONDUIT_API}/admin/security/client`, { platform });
 };
 
-export const deleteClientRequest = (_id) => {
+export const deleteClientRequest = (_id: string) => {
   return axios.delete(`${CONDUIT_API}/admin/security/client/${_id}`);
 };
 
-export const putCoreRequest = (data) => {
+export const putCoreRequest = (data: any) => {
   return axios.put(`${CONDUIT_API}/config/core`, data);
 };
 
@@ -25,18 +26,21 @@ export const getServiceAccounts = () => {
   return axios.get(`${CONDUIT_API}/admin/authentication/services`);
 };
 
-export const deleteServiceAccounts = (_id) => {
+export const deleteServiceAccounts = (_id: string) => {
   return axios.delete(`${CONDUIT_API}/admin/authentication/services/${_id}`);
 };
 
-export const createServiceAccount = (name) => {
+export const createServiceAccount = (name: string) => {
   return axios.post(`${CONDUIT_API}/admin/authentication/services`, { name });
 };
 
-export const refreshServiceAccount = (serviceId) => {
+export const refreshServiceAccount = (serviceId: string) => {
   return axios.put(`${CONDUIT_API}/admin/authentication/services`, { serviceId });
 };
 
-export const postNewAdminUser = (endpointData) => {
+export const postNewAdminUser = (endpointData: {
+  username: string;
+  password: string;
+}) => {
   return axios.post(`${CONDUIT_API}/admin/create`, endpointData);
 };
