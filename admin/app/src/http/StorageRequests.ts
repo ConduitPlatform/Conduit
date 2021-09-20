2bbe301bcea398d2d4c8690ed21227a2b89c2693
dasno <ep.giannakopoulos@gmail.com>
2021-09-20 14:08:13 +0300

    add: Storage requests types

--- admin/app/src/http/StorageRequests.ts
+++ admin/app/src/http/StorageRequests.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import { CONDUIT_API } from './requestsConfig';
+import { IStorageConfig } from '../models/storage/StorageModels';
 
 export const getStorageSettings = () => axios.get(`${CONDUIT_API}/admin/config/storage`);
 
-export const putStorageSettings = (storageData) =>
+export const putStorageSettings = (storageData: IStorageConfig) =>
   axios.put(`${CONDUIT_API}/admin/config/storage`, {
     ...storageData,
   });
