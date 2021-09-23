48abfb4e972594fba8ad854cf994046fd1e657b2
dasno <ep.giannakopoulos@gmail.com>
2021-09-23 12:40:42 +0300

    add: settings / storage global error / loading

--- admin/app/src/pages/storage.tsx
+++ admin/app/src/pages/storage.tsx
@@ -5,12 +5,6 @@ import React, { useEffect, useState } from 'react';
 import { privateRoute } from '../components/utils/privateRoute';
 import StorageFiles from '../components/storage/StorageFiles';
 import StorageSettings from '../components/storage/StorageSettings';
-import Snackbar from '@material-ui/core/Snackbar';
-import Backdrop from '@material-ui/core/Backdrop';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { makeStyles } from '@material-ui/core/styles';
-import Alert from '@material-ui/lab/Alert';
-import { SnackbarCloseReason } from '@material-ui/core/Snackbar/Snackbar';
 import { IStorageConfig } from '../models/storage/StorageModels';
 import {
   asyncGetStorageConfig,
@@ -18,35 +12,17 @@ import {
 } from '../redux/slices/storageSlice';
 import { useAppDispatch, useAppSelector } from '../redux/store';
 
-const useStyles = makeStyles((theme) => ({
-  backdrop: {
-    zIndex: theme.zIndex.drawer + 1,
-  },
-  snackBar: {
-    maxWidth: '80%',
-    width: 'auto',
-  },
-}));
 const tabs = [{ title: 'Files' }, { title: 'Settings' }];
 
 const Storage: React.FC = () => {
-  const classes = useStyles();
   const [selected, setSelected] = useState(0);
   const dispatch = useAppDispatch();
 
   const { config } = useAppSelector((state) => state.storageSlice.data);
-  const { loading, error } = useAppSelector((state) => state.storageSlice.meta);
-  const [snackbarOpen, setSnackbarOpen] = useState(false);
 
   useEffect(() => {
     dispatch(asyncGetStorageConfig());
   }, [dispatch]);
-
-  useEffect(() => {
-    if (error) {
-      setSnackbarOpen(true);
-    }
-  }, [error]);
 
   const handleChange = (event: React.ChangeEvent<any>, newValue: number) => {
     setSelected(newValue);
@@ -54,25 +30,6 @@ const Storage: React.FC = () => {
 
   const handleStorageSettings = (data: IStorageConfig) => {
     dispatch(asyncSaveStorageConfig(data));
-  };
-
-  const snackbarAlert = () => {
-    if (error) {
-      return (
-        <Alert variant={'filled'} severity="error">
-          {error ? error : 'Something went wrong!'}
-        </Alert>
-      );
-    } else {
-      return undefined;
-    }
-  };
-
-  const handleClose = (event: React.SyntheticEvent<any>, reason: SnackbarCloseReason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-    setSnackbarOpen(false);
   };
 
   return (
@@ -87,17 +44,6 @@ const Storage: React.FC = () => {
           <StorageSettings config={config} handleSave={handleStorageSettings} />
         </Box>
       </Box>
-      <Snackbar
-        open={snackbarOpen}
-        className={classes.snackBar}
-        autoHideDuration={6000}
-        onClose={handleClose}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}>
-        {snackbarAlert()}
-      </Snackbar>
-      <Backdrop open={loading} className={classes.backdrop}>
-        <CircularProgress color="secondary" />
-      </Backdrop>
     </>
   );
 };
