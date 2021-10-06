6a4558b7d2fd62542f7834ea3f0df0c9c1682db0
kkopanidis <kkopanidis@gmail.com>
2021-10-06 16:37:23 +0300

    add: ignore in doczrc

--- doczrc.js
+++ doczrc.js
@@ -1,4 +1,12 @@
 export default {
   dest: '/docs',
   base: '/conduit',
+  ignore: [
+    'readme.md',
+    'changelog.md',
+    'code_of_conduct.md',
+    'contributing.md',
+    'license.md',
+    'pull_request_template.md',
+  ],
 };
