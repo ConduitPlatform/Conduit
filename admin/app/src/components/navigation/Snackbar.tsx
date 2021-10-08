7c224ba1fba014de36092888093e8932791dbaac
dasno <ep.giannakopoulos@gmail.com>
2021-10-08 15:57:37 +0300

    remove: unused classes

--- admin/app/src/components/navigation/Snackbar.tsx
+++ admin/app/src/components/navigation/Snackbar.tsx
@@ -28,28 +28,6 @@ const useStyles = makeStyles((theme) => ({
   icons: {
     marginLeft: 'auto',
   },
-  expand: {
-    padding: '8px 8px',
-    transform: 'rotate(0deg)',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  collapse: {
-    padding: 16,
-  },
-  checkIcon: {
-    fontSize: 20,
-    color: '#b3b3b3',
-    paddingRight: 4,
-  },
-  button: {
-    padding: 0,
-    textTransform: 'none',
-  },
   error: {
     backgroundColor: theme.palette.error.main,
   },
