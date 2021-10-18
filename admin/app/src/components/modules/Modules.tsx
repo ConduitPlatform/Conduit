9dd83bc6bedf17d15d202d42b89b60417e4c159d
dasno <ep.giannakopoulos@gmail.com>
2021-10-18 13:11:11 +0300

    add: CustomListItem component

--- admin/app/src/components/modules/Modules.tsx
+++ admin/app/src/components/modules/Modules.tsx
@@ -1,55 +1,10 @@
 import React from 'react';
-import { Theme } from '@material-ui/core';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItem from '@material-ui/core/ListItem';
 import { Home } from '@material-ui/icons';
-import Link from 'next/link';
-import { makeStyles } from '@material-ui/core/styles';
 import { getModuleIcon, handleModuleNavigation } from './moduleUtils';
 import { useRouter } from 'next/router';
 import { useAppDispatch } from '../../redux/store';
 import { enqueueInfoNotification } from '../../utils/useNotifier';
-
-const useStyles = makeStyles((theme: Theme) => ({
-  listItem: {
-    minHeight: theme.spacing(4),
-    borderRadius: theme.spacing(0.5),
-    margin: theme.spacing(1, 0),
-    color: theme.palette.secondary.main,
-    borderWidth: 1,
-    paddingLeft: theme.spacing(0.5),
-    paddingRight: theme.spacing(0.5),
-    '&:hover': {
-      borderWidth: 1,
-    },
-    '&:focus': {
-      borderWidth: 1,
-    },
-    '&.Mui-selected': {
-      color: theme.palette.common.white,
-      borderWidth: 1,
-      '&:hover': {
-        background: theme.palette.secondary.dark,
-        borderWidth: 1,
-      },
-      '&:focus': {
-        background: theme.palette.secondary.dark,
-        borderWidth: 1,
-      },
-    },
-  },
-  listItemText: {
-    fontWeight: 'bold',
-    textTransform: 'capitalize',
-    fontSize: 12,
-  },
-  listItemIcon: {
-    minWidth: theme.spacing(4),
-    marginRight: theme.spacing(1),
-    color: 'inherit',
-  },
-}));
+import CustomListItem from '../navigation/CustomListItem';
 
 interface IModule {
   moduleName: string;
@@ -63,7 +18,6 @@ interface Props {
 }
 
 const Modules: React.FC<Props> = ({ modules, homeEnabled, itemSelected }) => {
-  const classes = useStyles();
   const router = useRouter();
   const dispatch = useAppDispatch();
 
@@ -78,14 +32,12 @@ const Modules: React.FC<Props> = ({ modules, homeEnabled, itemSelected }) => {
   return (
     <>
       {homeEnabled ? (
-        <Link href="/" passHref>
-          <ListItem button className={classes.listItem} selected={itemSelected === ''}>
-            <ListItemIcon className={classes.listItemIcon}>
-              <Home color={'inherit'} />
-            </ListItemIcon>
-            <ListItemText primary={'home'} classes={{ primary: classes.listItemText }} />
-          </ListItem>
-        </Link>
+        <CustomListItem
+          selected={itemSelected === ''}
+          icon={<Home color={'inherit'} />}
+          title="home"
+          onClick={() => handleItemClick('/', true)}
+        />
       ) : (
         <></>
       )}
@@ -93,20 +45,13 @@ const Modules: React.FC<Props> = ({ modules, homeEnabled, itemSelected }) => {
         modules.map((module, index) => {
           const currentUrl = handleModuleNavigation(module.moduleName);
           return (
-            <ListItem
-              button
-              className={classes.listItem}
+            <CustomListItem
               selected={itemSelected === module.moduleName}
+              icon={getModuleIcon(module.moduleName)}
+              title={module.moduleName}
+              onClick={() => handleItemClick(currentUrl, !!module.url)}
               key={index}
-              onClick={() => handleItemClick(currentUrl, !!module.url)}>
-              <ListItemIcon className={classes.listItemIcon}>
-                {getModuleIcon(module.moduleName)}
-              </ListItemIcon>
-              <ListItemText
-                primary={module.moduleName}
-                classes={{ primary: classes.listItemText }}
-              />
-            </ListItem>
+            />
           );
         })}
     </>
--- admin/app/src/components/navigation/Drawer.tsx
+++ admin/app/src/components/navigation/Drawer.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import { Paper, Theme, Typography } from '@material-ui/core';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import ListItem from '@material-ui/core/ListItem';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import { ExitToApp, Settings } from '@material-ui/icons';
-import Router from 'next/router';
+import Router, { useRouter } from 'next/router';
 import { asyncLogout } from '../../redux/slices/appAuthSlice';
 import { makeStyles } from '@material-ui/core/styles';
 import { useAppDispatch, useAppSelector } from '../../redux/store';
 import Modules from '../modules/Modules';
-import Link from 'next/link';
-import clsx from 'clsx';
+import CustomListItem from './CustomListItem';
 
 const useStyles = makeStyles((theme: Theme) => ({
   drawer: {
@@ -31,43 +28,6 @@ const useStyles = makeStyles((theme: Theme) => ({
     flexDirection: 'column',
     justifyContent: 'space-between',
   },
-  toolbar: theme.mixins.toolbar,
-  listItem: {
-    minHeight: theme.spacing(4),
-    borderRadius: theme.spacing(0.5),
-    marginBottom: theme.spacing(2),
-    color: theme.palette.secondary.main,
-    borderWidth: 1,
-    paddingLeft: theme.spacing(0.5),
-    paddingRight: theme.spacing(0.5),
-    '&:hover': {
-      borderWidth: 1,
-    },
-    '&:focus': {
-      borderWidth: 1,
-    },
-    '&.Mui-selected': {
-      color: theme.palette.common.white,
-      borderWidth: 1,
-      '&:hover': {
-        background: theme.palette.secondary.dark,
-        borderWidth: 1,
-      },
-      '&:focus': {
-        background: theme.palette.secondary.dark,
-        borderWidth: 1,
-      },
-    },
-  },
-  listItemText: {
-    fontWeight: 'bold',
-    fontSize: 12,
-  },
-  listItemIcon: {
-    minWidth: theme.spacing(4),
-    marginRight: theme.spacing(1),
-    color: 'inherit',
-  },
   logoutContainer: {
     margin: 0,
     paddingLeft: theme.spacing(1),
@@ -81,6 +41,7 @@ interface Props {
 const CustomDrawer: React.FC<Props> = ({ itemSelected, ...rest }) => {
   const classes = useStyles();
   const dispatch = useAppDispatch();
+  const router = useRouter();
 
   const { enabledModules, disabledModules } = useAppSelector((state) => state.appAuthSlice.data);
 
@@ -98,14 +59,12 @@ const CustomDrawer: React.FC<Props> = ({ itemSelected, ...rest }) => {
         <List component="nav">
           <Divider />
           <Modules modules={enabledModules} homeEnabled itemSelected={itemSelected} />
-          <Link href={`/settings/clientsdk`} passHref>
-            <ListItem button className={classes.listItem} selected={itemSelected === 'settings'}>
-              <ListItemIcon className={classes.listItemIcon}>
-                <Settings color={'inherit'} />
-              </ListItemIcon>
-              <ListItemText primary={'Settings'} classes={{ primary: classes.listItemText }} />
-            </ListItem>
-          </Link>
+          <CustomListItem
+            selected={itemSelected === 'settings'}
+            icon={<Settings color={'inherit'} />}
+            title="Settings"
+            onClick={() => router.replace('/settings/clientsdk')}
+          />
           <Divider />
           {disabledModules.length > 0 ? (
             <>
@@ -116,15 +75,12 @@ const CustomDrawer: React.FC<Props> = ({ itemSelected, ...rest }) => {
             <></>
           )}
         </List>
-        <ListItem
-          button
-          className={clsx(classes.listItem, classes.logoutContainer)}
-          onClick={handleLogout}>
-          <ListItemIcon className={classes.listItemIcon}>
-            <ExitToApp color={'inherit'} />
-          </ListItemIcon>
-          <ListItemText primary={'Log out'} classes={{ primary: classes.listItemText }} />
-        </ListItem>
+        <CustomListItem
+          icon={<ExitToApp color={'inherit'} />}
+          title="Log out"
+          onClick={() => handleLogout()}
+          className={classes.logoutContainer}
+        />
       </div>
     </Paper>
   );
