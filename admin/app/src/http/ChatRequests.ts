9490a0737027bd603ba1825a1ab851883af6b81c
dasno <ep.giannakopoulos@gmail.com>
2021-11-03 14:10:06 +0200

    add: chat requests

---
+++ admin/app/src/http/ChatRequests.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { CONDUIT_API } from './requestsConfig';
+
+export const createChatRoom = (params: { name: string; participants: string[] }) =>
+  axios.post(`${CONDUIT_API}/admin/chat/room`, {
+    ...params,
+  });
+
+export const getChatRooms = (params: { skip: number; limit: number; search: string }) =>
+  axios.get(`${CONDUIT_API}/admin/chat/rooms`, {
+    params: {
+      ...params,
+    },
+  });
+
+export const deleteChatRooms = (params: { ids: string[] }) =>
+  axios.delete(`${CONDUIT_API}/admin/chat/rooms`, {
+    data: {
+      ...params,
+    },
+  });
+
+export const getChatMessages = (params: {
+  skip: number;
+  limit: number;
+  senderId: string;
+  roomId: string;
+}) =>
+  axios.get(`${CONDUIT_API}/admin/chat/messages`, {
+    params: {
+      ...params,
+    },
+  });
+
+export const deleteChatMessages = (params: { ids: string[] }) =>
+  axios.delete(`${CONDUIT_API}/admin/chat/messages`, {
+    data: {
+      ...params,
+    },
+  });
