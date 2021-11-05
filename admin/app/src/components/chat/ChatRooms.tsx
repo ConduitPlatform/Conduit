74844facd334505310989a251122c22b3a5ca1d3
dasno <ep.giannakopoulos@gmail.com>
2021-11-05 12:09:26 +0200

    add: useAppSelector

--- admin/app/src/components/chat/ChatRooms.tsx
+++ admin/app/src/components/chat/ChatRooms.tsx
@@ -3,7 +3,7 @@ import { Box, Button, Paper } from '@material-ui/core';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import { makeStyles } from '@material-ui/core/styles';
-import { useAppDispatch } from '../../redux/store';
+import { useAppDispatch, useAppSelector } from '../../redux/store';
 import { asyncGetChatRooms } from '../../redux/slices/chatSlice';
 import ChatRoomPanel from './ChatRoomPanel';
 import AddCircleOutline from '@material-ui/icons/AddCircleOutline';
@@ -48,6 +48,10 @@ const participants = [
 const ChatRooms: React.FC = () => {
   const classes = useStyles();
   const dispatch = useAppDispatch();
+
+  const {
+    chatRooms: { data, count },
+  } = useAppSelector((state) => state.chatSlice.data);
 
   const [selected, setSelected] = useState(0);
   const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
