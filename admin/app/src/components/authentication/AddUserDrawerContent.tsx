09d00acc26bd22933430cb32c3a69a2ef4eb1518
KostasFeg <classcreator21@outlook.com>
2021-11-10 16:11:05 +0200

    AddUsers implementation with new rhf components

--- admin/app/src/components/authentication/AddUserDrawerContent.tsx
+++ admin/app/src/components/authentication/AddUserDrawerContent.tsx
@@ -1,16 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
 import SaveIcon from '@material-ui/icons/Save';
-import LockOpenIcon from '@material-ui/icons/LockOpen';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import AccountCircle from '@material-ui/icons/AccountCircle';
 import addUser from '../../assets/svgs/addUser.svg';
 import Grid from '@material-ui/core/Grid';
 import Image from 'next/image';
 import Container from '@material-ui/core/Container';
-import { useForm, Controller } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import { FormInputText } from '../common/RHFormComponents/RHFInputText';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -40,9 +37,20 @@ interface Props {
   handleNewUserDispatch: (values: { password: string; email: string }) => void;
 }
 
+interface NewUserInputs {
+  email: string;
+  password: string;
+}
+
+const defaultValues = {
+  email: '',
+  password: '',
+};
+
 const NewUserModal: React.FC<Props> = ({ handleNewUserDispatch }) => {
   const classes = useStyles();
-  const { handleSubmit, control } = useForm();
+  const methods = useForm<NewUserInputs>({ defaultValues: defaultValues });
+  const { handleSubmit, control } = methods;
 
   const onSubmit = (data: { password: string; email: string }) => {
     handleNewUserDispatch(data);
@@ -51,76 +59,28 @@ const NewUserModal: React.FC<Props> = ({ handleNewUserDispatch }) => {
   return (
     <div className={classes.root} style={{ marginTop: '150px' }}>
       <h3 style={{ textAlign: 'center' }}>Add a new user</h3>
-      <form onSubmit={handleSubmit(onSubmit)} style={{}}>
-        <Container className={classes.root} maxWidth="sm">
-          <Grid container alignItems="center" className={classes.root} spacing={2}>
-            <Grid item sm={12}>
-              <Controller
-                name="email"
-                control={control}
-                defaultValue=""
-                render={({ field: { onChange, value }, fieldState: { error } }) => (
-                  <TextField
-                    label="Email"
-                    variant="outlined"
-                    value={value}
-                    fullWidth
-                    onChange={onChange}
-                    error={!!error}
-                    helperText={error ? error.message : null}
-                    type="email"
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="start">
-                          <AccountCircle />
-                        </InputAdornment>
-                      ),
-                    }}
-                  />
-                )}
-                rules={{ required: 'Email required' }}
-              />
-            </Grid>
-            <Grid item sm={12}>
-              <Controller
-                name="password"
-                control={control}
-                defaultValue=""
-                render={({ field: { onChange, value }, fieldState: { error } }) => (
-                  <TextField
-                    label="Password"
-                    variant="outlined"
-                    value={value}
-                    fullWidth
-                    onChange={onChange}
-                    error={!!error}
-                    helperText={error ? error.message : null}
-                    type="password"
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="start">
-                          <LockOpenIcon />
-                        </InputAdornment>
-                      ),
-                    }}
-                  />
-                )}
-                rules={{ required: 'Password required' }}
-              />
-            </Grid>
-            <Grid item>
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                size="large"
-                startIcon={<SaveIcon />}>
-                Save
-              </Button>
-            </Grid>
+      <Container className={classes.root} maxWidth="sm">
+        <Grid container alignItems="center" className={classes.root} spacing={2}>
+          <Grid item sm={12}>
+            <FormInputText name="email" control={control} label="Username/Email" />
           </Grid>
-        </Container>
-      </form>
+          <Grid item sm={12}>
+            <FormInputText name="password" control={control} label="Password" />
+          </Grid>
+          <Grid item>
+            <Button
+              onClick={handleSubmit(onSubmit)}
+              type="submit"
+              variant="contained"
+              color="primary"
+              size="large"
+              startIcon={<SaveIcon />}>
+              Save
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+
       <div className={classes.centeredImg}>
         <Image src={addUser} width="200px" alt="addUser" />
       </div>
