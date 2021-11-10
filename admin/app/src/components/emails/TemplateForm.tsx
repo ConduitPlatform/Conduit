234c6187082c7eb821754ed493b7437d25b0b723
KostasFeg <classcreator21@outlook.com>
2021-11-10 16:40:18 +0200

    Implementation of newTemplate form for emails on rhf

---
+++ admin/app/src/components/emails/TemplateForm.tsx
@@ -0,0 +1,74 @@
+import { Button, Container, Grid, TextField } from '@material-ui/core';
+import React, { FC } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import TemplateEditor from './TemplateEditor';
+import sharedClasses from '../common/sharedClasses';
+import { FormInputText } from '../common/RHFormComponents/RHFInputText';
+import { EmailData, SendEmailData } from '../../models/emails/EmailModels';
+
+interface ITemplateForm {
+  name: string;
+  sender: string;
+  subject: string;
+  body: string;
+}
+
+interface Props {
+  preloadedValues: EmailData;
+  handleSubmitData: (data: any) => void;
+}
+
+const TemplateForm: FC<Props> = ({ preloadedValues, handleSubmitData }) => {
+  const classes = sharedClasses();
+
+  const methods = useForm<ITemplateForm>({ defaultValues: preloadedValues });
+
+  const { handleSubmit, reset, control } = methods;
+
+  const onSubmit = (data: SendEmailData) => {
+    handleSubmitData({ ...data });
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} style={{}}>
+      <Container className={classes.root} maxWidth="xl">
+        <Grid container alignItems="center" className={classes.root} spacing={2}>
+          <Grid item sm={12}>
+            <FormInputText name="name" control={control} label="Name" />
+          </Grid>
+          <Grid item sm={12}>
+            <FormInputText name="sender" control={control} label="Sender*" />
+          </Grid>
+          <Grid item sm={12}>
+            <FormInputText name="subject" control={control} label="Subject" />
+          </Grid>
+          <Grid item sm={12}>
+            <Controller
+              name="body"
+              control={control}
+              defaultValue=""
+              render={({ field: { onChange, value }, fieldState: { error } }) => (
+                <TemplateEditor value={value} setValue={onChange} />
+              )}
+              rules={{ required: 'Template body required' }}
+            />
+          </Grid>
+          <Grid container item xs={12} justify="space-around" style={{ marginTop: '35px' }}>
+            <Grid item>
+              <Button type="submit" variant="contained" color="primary" size="large">
+                Save
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button onClick={() => reset()} variant="contained" color="primary" size="large">
+                Cancel
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </form>
+  );
+};
+
+export default TemplateForm;
