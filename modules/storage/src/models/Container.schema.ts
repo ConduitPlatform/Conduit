176d9b3fbce95db7ec40336ec13f7a6debcf1068
kon14 <konferetos@tutanota.com>
2021-11-11 10:52:46 +0200

    refactor(storage): convert schemas to ConduitActiveSchema format

---
+++ modules/storage/src/models/Container.schema.ts
@@ -0,0 +1,48 @@
+import {
+  ConduitActiveSchema,
+  DatabaseProvider,
+  TYPE,
+} from '@quintessential-sft/conduit-grpc-sdk';
+
+const schema = {
+  _id: TYPE.ObjectId,
+  name: {
+    type: TYPE.String,
+    required: true,
+    unique: true,
+    systemRequired: true,
+  },
+  isPublic: {
+    type: TYPE.Boolean,
+    default: false,
+  },
+  createdAt: TYPE.Date,
+  updatedAt: TYPE.Date,
+};
+const schemaOptions = {
+  timestamps: true,
+  systemRequired: true,
+};
+const collectionName = undefined;
+
+export class _StorageContainer extends ConduitActiveSchema<_StorageContainer> {
+  private static _instance: _StorageContainer;
+  _id!: string;
+  name!: string;
+  isPublic!: boolean;
+  createdAt!: Date;
+  updatedAt!: Date;
+
+  private constructor(database: DatabaseProvider) {
+    super(database, _StorageContainer.name, schema, schemaOptions, collectionName);
+  }
+
+  static getInstance(database?: DatabaseProvider) {
+    if (_StorageContainer._instance) return _StorageContainer._instance;
+    if (!database) {
+      throw new Error('No database instance provided!');
+    }
+    _StorageContainer._instance = new _StorageContainer(database);
+    return _StorageContainer._instance;
+  }
+}
