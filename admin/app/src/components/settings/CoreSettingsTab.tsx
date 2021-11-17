6ab9d4289bbc3161025fb4c224240d88e1bc5cfc
Konstantinos Fegoulis <56806349+KostasFeg@users.noreply.github.com>
2021-11-17 12:09:01 +0200

    refactor(settingsForms): changed according to the rest of the modules

--- admin/app/src/components/settings/CoreSettingsTab.tsx
+++ admin/app/src/components/settings/CoreSettingsTab.tsx
@@ -2,10 +2,10 @@ import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 import { Grid, Container, Button } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
-import { useForm } from 'react-hook-form';
-import { FormSelect } from '../common/FormComponents/FormSelect';
-import { FormInput } from '../common/FormComponents/FormInput';
-import { FormSwitch } from '../common/FormComponents/RHFSwitch';
+import { FormProvider, useForm } from 'react-hook-form';
+import { FormInputSelect } from '../common/FormComponents/FormInputSelect';
+import { FormInputText } from '../common/FormComponents/FormInputText';
+import { FormInputSwitch } from '../common/FormComponents/FormInputSwitch';
 
 const useStyles = makeStyles(() => ({
   textSpacing: {
@@ -35,19 +35,19 @@ const CoreSettingsTab: React.FC = () => {
   // const dispatch = useDispatch();
   const methods = useForm<CoreSettings>({ defaultValues: initialStates });
 
-  const { reset, control } = methods;
+  const { reset } = methods;
 
-  // const onSaveClick = (data: CoreSettings) => {
-  //   // const data = {
-  //   //   port: port,
-  //   //   hostUrl: url,
-  //   //   rest: toggleRest,
-  //   //   graphql: toggleGraphQL,
-  //   //   env: selectedEnum,
-  //   // };
-  //   //dispatch(putCoreSettings(data));
-  //   console.log(data);
-  // };
+  const onSaveClick = (data: CoreSettings) => {
+    // const data = {
+    //   port: port,
+    //   hostUrl: url,
+    //   rest: toggleRest,
+    //   graphql: toggleGraphQL,
+    //   env: selectedEnum,
+    // };
+    //dispatch(putCoreSettings(data));
+    console.log(data);
+  };
 
   const selectOptions = [
     { name: 'development', label: 'development' },
@@ -58,69 +58,72 @@ const CoreSettingsTab: React.FC = () => {
   return (
     <Container>
       <Grid container justify={'center'}>
-        <form>
-          <Grid item xs={12}>
-            <Typography variant={'h6'}>Core settings</Typography>
-            <Typography variant={'subtitle1'}>
-              Below you can see information about the Conduit location
-            </Typography>
-          </Grid>
-          <Grid item xs={12} container alignItems={'center'}>
-            <Typography variant={'subtitle2'} className={classes.textSpacing}>
-              Environment:
-            </Typography>
-            <FormSelect
-              name="selectedEnum"
-              control={control}
-              label="Environment"
-              options={selectOptions}
-            />
-          </Grid>
-          <Grid item xs={12} style={{ marginTop: 16 }} container wrap={'nowrap'}>
-            <Grid
-              item
-              xs={8}
-              sm={4}
-              container
-              alignItems={'center'}
-              wrap={'nowrap'}
-              style={{ marginRight: 32 }}>
-              <Typography component={'span'} variant={'subtitle2'} className={classes.textSpacing}>
-                Url:
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(onSaveClick)}>
+            <Grid item xs={12}>
+              <Typography variant={'h6'}>Core settings</Typography>
+              <Typography variant={'subtitle1'}>
+                Below you can see information about the Conduit location
               </Typography>
-              <FormInput name="url" label="URL" control={control} />
             </Grid>
-            <Grid item container alignItems={'center'} wrap={'nowrap'}>
-              <Typography component={'span'} variant={'subtitle2'} className={classes.textSpacing}>
-                Port:
+            <Grid item xs={12} container alignItems={'center'}>
+              <Typography variant={'subtitle2'} className={classes.textSpacing}>
+                Environment:
               </Typography>
-              <FormInput name="port" label="Port" control={control} />
+              <FormInputSelect name="selectedEnum" label="Environment" options={selectOptions} />
             </Grid>
-          </Grid>
-          <Grid item xs={12} style={{ marginTop: 32 }}>
-            <Typography variant={'h6'}>Transport section</Typography>
-          </Grid>
-          <Grid item xs={12} container alignItems={'center'}>
-            <Typography variant={'subtitle1'}>Toggle Rest:</Typography>
-            <FormSwitch name="toggleRest" control={control} />
-          </Grid>
-          <Grid item xs={12} container alignItems={'center'}>
-            <Typography variant={'subtitle1'}>Toggle GraphQL:</Typography>
-            <FormSwitch name="toggleGraphQL" control={control} />
-          </Grid>
-          <Grid item xs={12} style={{ marginTop: 32 }}>
-            <Button
-              style={{ marginRight: 32 }}
-              placeholder={'Cancel'}
-              onClick={() => reset()}
-              variant={'contained'}>
-              Cancel
-            </Button>
-            <Button placeholder={'Save'} variant={'contained'} color={'primary'} type="submit">
-              Save
-            </Button>
-          </Grid>
-        </form>
+            <Grid item xs={12} style={{ marginTop: 16 }} container wrap={'nowrap'}>
+              <Grid
+                item
+                xs={8}
+                sm={4}
+                container
+                alignItems={'center'}
+                wrap={'nowrap'}
+                style={{ marginRight: 32 }}>
+                <Typography
+                  component={'span'}
+                  variant={'subtitle2'}
+                  className={classes.textSpacing}>
+                  Url:
+                </Typography>
+                <FormInputText name="url" label="URL" />
+              </Grid>
+              <Grid item container alignItems={'center'} wrap={'nowrap'}>
+                <Typography
+                  component={'span'}
+                  variant={'subtitle2'}
+                  className={classes.textSpacing}>
+                  Port:
+                </Typography>
+                <FormInputText name="port" label="Port" />
+              </Grid>
+            </Grid>
+            <Grid item xs={12} style={{ marginTop: 32 }}>
+              <Typography variant={'h6'}>Transport section</Typography>
+            </Grid>
+            <Grid item xs={12} container alignItems={'center'}>
+              <Typography variant={'subtitle1'}>Toggle Rest:</Typography>
+              <FormInputSwitch name="toggleRest" />
+            </Grid>
+            <Grid item xs={12} container alignItems={'center'}>
+              <Typography variant={'subtitle1'}>Toggle GraphQL:</Typography>
+              <FormInputSwitch name="toggleGraphQL" />
+            </Grid>
+            <Grid item xs={12} style={{ marginTop: 32 }}>
+              <Button
+                style={{ marginRight: 32 }}
+                placeholder={'Cancel'}
+                onClick={() => reset()}
+                variant={'contained'}>
+                Cancel
+              </Button>
+              <Button placeholder={'Save'} variant={'contained'} color={'primary'} type="submit">
+                Save
+              </Button>
+            </Grid>
+          </form>
+        </FormProvider>
       </Grid>
     </Container>
   );
--- admin/app/src/components/settings/CreateNewUserTab.tsx
+++ admin/app/src/components/settings/CreateNewUserTab.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Button, Container, Grid, Theme, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { INewAdminUser } from '../../models/settings/SettingsModels';
-import { FormInput } from '../common/FormComponents/FormInput';
+import { FormInputText } from '../common/FormComponents/FormInputText';
 import { useDispatch } from 'react-redux';
 import { asyncCreateAdminUser } from '../../redux/slices/settingsSlice';
-import { useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 
 const useStyles = makeStyles((theme: Theme) => ({
   title: {
@@ -28,7 +28,7 @@ const CreateNewUserTab: React.FC = () => {
   const dispatch = useDispatch();
   const methods = useForm<INewAdminUser>({ defaultValues: { username: '', password: '' } });
 
-  const { handleSubmit, reset, control } = methods;
+  const { reset } = methods;
 
   const handleRegister = (values: INewAdminUser) => {
     dispatch(asyncCreateAdminUser(values));
@@ -43,33 +43,35 @@ const CreateNewUserTab: React.FC = () => {
             Create New User
           </Typography>
         </Grid>
-        <form onSubmit={handleSubmit(handleRegister)} className={classes.form}>
-          <FormInput
-            name="username"
-            control={control}
-            required="Username is required"
-            minimumLength={5}
-            minLengthMsg="Username should be 5 characters or longer"
-            label="Username"
-          />
-          <div style={{ marginTop: '10px' }}> </div>
-          <FormInput
-            name="password"
-            control={control}
-            required="Password is required"
-            minimumLength={5}
-            minLengthMsg="Password should be 5 characters or longer"
-            label="Password"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.button}>
-            Create New User
-          </Button>
-        </form>
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(handleRegister)} className={classes.form}>
+            <FormInputText
+              name="username"
+              rules={{
+                required: 'Username is required',
+                minLength: { value: 5, message: 'Username should be 5 characters or longer' },
+              }}
+              label="Username"
+            />
+            <div style={{ marginTop: '10px' }}> </div>
+            <FormInputText
+              name="password"
+              rules={{
+                required: 'Password is required',
+                minLength: { value: 5, message: 'Password should be 5 characters or longer' },
+              }}
+              label="Password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.button}>
+              Create New User
+            </Button>
+          </form>
+        </FormProvider>
       </Grid>
     </Container>
   );
