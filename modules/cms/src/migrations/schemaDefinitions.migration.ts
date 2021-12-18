5c980336d8cf54cfb98871922b328e0fe871d55e
kon14 <konferetos@tutanota.com>
2021-12-18 21:43:59 +0200

    fix(cms): schemaDefinitions migration
    Fix migration for existing DeclaredSchemas missing cms metadata.
    Prevent error output on non-existent SchemaDefinitions schema.

--- modules/cms/src/migrations/schemaDefinitions.migration.ts
+++ modules/cms/src/migrations/schemaDefinitions.migration.ts
@@ -4,37 +4,47 @@ import { status } from '@grpc/grpc-js';
 import { isNil } from 'lodash';
 
 export async function migrateSchemaDefinitions(grpcSdk: ConduitGrpcSdk) {
-  // make sure the schema is initialized properly
+  const schemas = await grpcSdk.databaseProvider!.getSchemas()
+    .catch((e: Error) => { throw new GrpcError(status.INTERNAL, e.message); });
+  if (schemas.filter((schema: any) => schema.name === 'SchemaDefinitions').length === 0)
+    return;
+
+  // Initialize DeclaredSchema ConduitActiveSchema
   _DeclaredSchema.getInstance(grpcSdk.databaseProvider!);
+
   const schemaDefinitions: any = await grpcSdk
     .databaseProvider!.findMany('SchemaDefinitions', {})
     .catch((e: Error) => {
       throw new GrpcError(status.INTERNAL, e.message);
     });
+
+  // Migrate SchemaDefinitions to DeclaredSchemas
   if (!isNil(schemaDefinitions)) {
-    // Migrate SchemaDefinitions to DeclaredSchemas
     for (const schema of schemaDefinitions) {
       const declaredSchema = await _DeclaredSchema
         .getInstance()
         .findOne({ name: schema.name });
-      if (isNil(declaredSchema)) {
-        const modelOptions = {
-          ...JSON.parse(schema.modelOptions),
-          conduit: {
-            cms: {
-              authentication: schema.authentication,
-              crudOperations: schema.crudOperations,
-              enabled: schema.enabled,
-            },
+      let modelOptions = {
+        conduit: {
+          cms: {
+            authentication: schema.authentication,
+            crudOperations: schema.crudOperations,
+            enabled: schema.enabled,
           },
-        };
-        let newSchema = new ConduitSchema(schema.name, schema.fields, modelOptions);
-        await grpcSdk.databaseProvider!.createSchemaFromAdapter(newSchema);
+        }
       }
+      if (isNil(declaredSchema)) {
+        // No corresponding DeclaredSchema
+        modelOptions = { ...modelOptions, ...JSON.parse(schema.modelOptions) };
+      } else if (!('conduit' in declaredSchema.modelOptions)) {
+        // DeclaredSchema exists, missing metadata
+        modelOptions = { ...modelOptions, ...declaredSchema.modelOptions! };
+      }
+      const newSchema = new ConduitSchema(schema.name, schema.fields, modelOptions);
+      await grpcSdk.databaseProvider!.createSchemaFromAdapter(newSchema);
     }
-    // Delete SchemaDefinitions schema entry in DeclaredSchemas
-    await grpcSdk.databaseProvider!.deleteOne('_DeclaredSchema', { name: 'SchemaDefinitions' });
-    // Delete SchemaDefinitions collection/table // TODO: Requires handling systemRequired check in adapters
+
+    // Delete SchemaDefinitions // TODO: Requires handling systemRequired check in adapters
     // await grpcSdk.databaseProvider!.deleteSchema('SchemaDefinitions', true);
   }
 }
