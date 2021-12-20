5ec7ac7333762c2ec97c57cc1939c3be5e806bb1
kon14 <konferetos@tutanota.com>
2021-12-20 17:38:36 +0200

    fix(database,cms): migrations [Guided Migration]
    MIGRATION INSTRUCTIONS:
    Bring up the Database module before CMS.
    modelOptions.migration should run before schemaDefinitions.migration.

--- modules/cms/src/migrations/schemaDefinitions.migration.ts
+++ modules/cms/src/migrations/schemaDefinitions.migration.ts
@@ -1,7 +1,7 @@
-import ConduitGrpcSdk, { ConduitSchema, GrpcError } from '@quintessential-sft/conduit-grpc-sdk';
+import ConduitGrpcSdk, { ConduitSchema, ConduitModelOptions, GrpcError } from '@quintessential-sft/conduit-grpc-sdk';
 import { _DeclaredSchema } from '../models';
 import { status } from '@grpc/grpc-js';
-import { isNil } from 'lodash';
+import { isNil, merge } from 'lodash';
 
 export async function migrateSchemaDefinitions(grpcSdk: ConduitGrpcSdk) {
   const schemas = await grpcSdk.databaseProvider!.getSchemas()
@@ -24,21 +24,27 @@ export async function migrateSchemaDefinitions(grpcSdk: ConduitGrpcSdk) {
       const declaredSchema = await _DeclaredSchema
         .getInstance()
         .findOne({ name: schema.name });
-      let modelOptions = {
-        conduit: {
-          cms: {
-            authentication: schema.authentication,
-            crudOperations: schema.crudOperations,
-            enabled: schema.enabled,
-          },
+      const cmsMetadata = {
+        cms: {
+          authentication: schema.authentication,
+          crudOperations: schema.crudOperations,
+          enabled: schema.enabled,
         }
-      }
-      if (isNil(declaredSchema)) {
-        // No corresponding DeclaredSchema
-        modelOptions = { ...modelOptions, ...JSON.parse(schema.modelOptions) };
-      } else if (!('conduit' in declaredSchema.modelOptions)) {
+      };
+      let modelOptions: ConduitModelOptions = merge(
+        JSON.parse(schema.modelOptions),
+        { conduit: cmsMetadata },
+      );
+      if ((declaredSchema) && (
+        !declaredSchema.modelOptions ||
+        !declaredSchema.modelOptions.conduit ||
+        !('cms' in declaredSchema.modelOptions.conduit)
+      )) {
         // DeclaredSchema exists, missing metadata
-        modelOptions = { ...modelOptions, ...declaredSchema.modelOptions! };
+        modelOptions =
+          declaredSchema.modelOptions // possibly undefined
+          ? merge(declaredSchema.modelOptions, modelOptions)
+          : modelOptions;
       }
       const newSchema = new ConduitSchema(schema.name, schema.fields, modelOptions);
       await grpcSdk.databaseProvider!.createSchemaFromAdapter(newSchema);
--- modules/database/src/migrations/modelOptions.migration.ts
+++ modules/database/src/migrations/modelOptions.migration.ts
@@ -1,12 +1,17 @@
-import { _DeclaredSchema } from '../models/DeclaredSchema.schema';
+import { _DeclaredSchema } from '../models';
 
 export async function migrateModelOptions() {
   const documents = await _DeclaredSchema.getInstance()
-    .findMany({ modelOptions: { $type: 'string' } })
+    .findMany({ 'modelOptions.conduit': { $exists: false } })
     .catch((e: Error) => { Promise.reject(e.message); });
 
   for (const schema of (documents as _DeclaredSchema[])) {
-    const newModelOptions = JSON.parse((schema.modelOptions as string));
+    let newModelOptions: any = { conduit: {} };
+    try { // modelOptions could be {}
+      newModelOptions = { ...newModelOptions, ...JSON.parse((schema.modelOptions as string)) };
+    } catch {
+      newModelOptions = { ...newModelOptions, ...schema.modelOptions };
+    }
     await _DeclaredSchema.getInstance().findByIdAndUpdate(schema._id, {
       modelOptions: newModelOptions,
     }).catch((err: Error) => {
