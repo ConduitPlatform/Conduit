6c642b79f0398e506dbb0dfe76b489ec143eae3c
kon14 <konferetos@tutanota.com>
2021-12-20 10:40:18 +0200

    chore(database): cleanup modelOptions migration

--- modules/database/src/migrations/modelOptions.migration.ts
+++ modules/database/src/migrations/modelOptions.migration.ts
@@ -1,16 +1,12 @@
 import { _DeclaredSchema } from '../models/DeclaredSchema.schema';
-import { isNil } from 'lodash';
 
 export async function migrateModelOptions() {
-  let errorMessage: string | null = null;
-  const documents: any = await _DeclaredSchema.getInstance().findMany({})
-    .catch((e: Error) => (errorMessage = e.message));
-  if (!isNil(errorMessage)) {
-    return Promise.reject(errorMessage);
-  }
-  for (const schema of documents) {
-    if (typeof schema.modelOptions !== 'string') continue;
-    const newModelOptions = JSON.parse(schema.modelOptions);
+  const documents = await _DeclaredSchema.getInstance()
+    .findMany({ modelOptions: { $type: 'string' } })
+    .catch((e: Error) => { Promise.reject(e.message); });
+
+  for (const schema of (documents as _DeclaredSchema[])) {
+    const newModelOptions = JSON.parse((schema.modelOptions as string));
     await _DeclaredSchema.getInstance().findByIdAndUpdate(schema._id, {
       modelOptions: newModelOptions,
     }).catch((err: Error) => {
