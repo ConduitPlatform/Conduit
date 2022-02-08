c642d75889b0b2f88e5aaac4ddc8cfedd66ef66b
Konstantinos Kopanidis <kkopanidis@users.noreply.github.com>
2022-02-08 11:37:54 +0200

    Database internal communication (#17)
    * fix(database): DeclaredSchema usages
    * refactor(database): change DeclaredSchema and migration initializations to protect race conditions

--- modules/database/src/migrations/modelOptions.migration.ts
+++ modules/database/src/migrations/modelOptions.migration.ts
@@ -1,20 +1,22 @@
-import { _DeclaredSchema } from '../models';
+import { DatabaseAdapter } from '../adapters/DatabaseAdapter';
+import { MongooseSchema } from '../adapters/mongoose-adapter/MongooseSchema';
+import { SequelizeSchema } from '../adapters/sequelize-adapter/SequelizeSchema';
+import { IDeclaredSchema } from '../interfaces/DeclaredSchema';
 
-export async function migrateModelOptions() {
-  const documents = await _DeclaredSchema.getInstance()
-    .findMany({ 'modelOptions.conduit': { $exists: false } })
-    .catch((e: Error) => { Promise.reject(e.message); });
+export async function migrateModelOptions(adapter: DatabaseAdapter<MongooseSchema | SequelizeSchema>) {
+  const documents = await adapter.getSchemaModel('_DeclaredSchema').model
+    .findMany(JSON.stringify({ 'modelOptions.conduit': { $exists: false } }));
 
-  for (const schema of (documents as _DeclaredSchema[])) {
+  for (const schema of (documents as IDeclaredSchema[])) {
     let newModelOptions: any = { conduit: {} };
     try { // modelOptions could be {}
       newModelOptions = { ...newModelOptions, ...JSON.parse((schema.modelOptions as string)) };
     } catch {
       newModelOptions = { ...newModelOptions, ...schema.modelOptions };
     }
-    await _DeclaredSchema.getInstance().findByIdAndUpdate(schema._id, {
+    await adapter.getSchemaModel('_DeclaredSchema').model.findByIdAndUpdate(schema._id, JSON.stringify({
       modelOptions: newModelOptions,
-    }).catch((err: Error) => {
+    })).catch((err: Error) => {
       console.log(err);
     });
   }
