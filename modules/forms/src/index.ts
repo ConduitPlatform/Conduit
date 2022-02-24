2d9777f277a53c474b5437588dcc3780c671d69f
Konstantinos Feretos <konferetos@tutanota.com>
2022-02-24 11:25:14 +0200

    ManagedModule Refactor for Forms (#52)
    * refactor(forms): use ManagedModule
    
    * fix(forms)!: GetFormReplies route expects form id as query param
    
    BREAKING CHANGE:
    GetFormReplies now properly accepts form id as a url param

--- modules/forms/src/index.ts
+++ modules/forms/src/index.ts
@@ -1,32 +1,6 @@
-import ConduitGrpcSdk from '@conduitplatform/grpc-sdk';
+import { ModuleManager } from "@conduitplatform/grpc-sdk";
 import FormsModule from './Forms';
 
-if (!process.env.CONDUIT_SERVER) {
-  throw new Error('Conduit server URL not provided');
-}
-
-const serviceAddress = process.env.SERVICE_IP ? process.env.SERVICE_IP.split(':')[0] : '0.0.0.0';
-const servicePort = process.env.SERVICE_IP ? process.env.SERVICE_IP.split(':')[1] : undefined;
-
-let grpcSdk = new ConduitGrpcSdk(process.env.CONDUIT_SERVER, 'forms');
-let forms = new FormsModule(grpcSdk);
-forms
-  .initialize(servicePort)
-  .then(() => {
-    let url =
-      (process.env.REGISTER_NAME === 'true' ? 'forms:' : `${ serviceAddress }:`) +
-      forms.port;
-    return grpcSdk.config.registerModule('forms', url);
-  })
-  .catch((err: Error) => {
-    console.log('Failed to initialize server');
-    console.error(err);
-    process.exit(-1);
-  })
-  .then(() => {
-    return forms.activate();
-  })
-  .catch((err: Error) => {
-    console.log('Failed to activate module');
-    console.error(err);
-  });
+const forms = new FormsModule();
+const moduleManager = new ModuleManager(forms);
+moduleManager.start();
