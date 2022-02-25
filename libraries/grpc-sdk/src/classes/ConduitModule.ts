a605a2a866670cb321264844d44a7d7e73a1c49d
Konstantinos Kopanidis <kkopanidis@users.noreply.github.com>
2022-02-25 18:15:29 +0200

    Dynamic GRPC modules (#47)
    * refactor(grpc-sdk): separate function in ConduitModule for client re-connection
    feat(grpc-sdk): allow adding extra modules that are not included in the build
    
    * Update index.ts
    
    * fix(grpc-sdk): remove references to deleted CMS module
    
    Co-authored-by: Konstantinos Feretos <konferetos@tutanota.com>

--- libraries/grpc-sdk/src/classes/ConduitModule.ts
+++ libraries/grpc-sdk/src/classes/ConduitModule.ts
@@ -4,9 +4,10 @@ import { Channel, Client, createChannel, createClientFactory } from 'nice-grpc';
 
 export class ConduitModule<T extends CompatServiceDefinition> {
   active: boolean = false;
-  protected client?: Client<T>;
+  private _client?: Client<T>;
   protected channel?: Channel;
   protected protoPath?: string;
+  protected type?: T;
   protected readonly _name: string;
   protected readonly _url: string;
 
@@ -21,25 +22,33 @@ export class ConduitModule<T extends CompatServiceDefinition> {
   }
 
   initializeClient(type: T): Client<T> {
-    if (this.client) return this.client;
+    if (this._client) return this._client;
+    this.type = type;
+    this.openConnection();
+    return this._client!;
+  }
+
+  openConnection() {
     this.channel = createChannel(this._url, undefined, {
       'grpc.max_receive_message_length': 1024 * 1024 * 100,
       'grpc.max_send_message_length': 1024 * 1024 * 100,
     });
-    this.client = createClientFactory()
+    this._client = createClientFactory()
       .use(getModuleNameInterceptor(this._name))
       .create(
-        type,
+        this.type!,
         this.channel,
       );
     this.active = true;
-    return this.client;
+  }
+
+  get client(): Client<T> | undefined{
+    return this._client;
   }
 
   closeConnection() {
     if (!this.channel) return;
     this.channel.close();
-    this.client = undefined;
     this.channel = undefined;
     this.active = false;
   }
--- libraries/grpc-sdk/src/index.ts
+++ libraries/grpc-sdk/src/index.ts
@@ -15,13 +15,16 @@ import Crypto from 'crypto';
 import { EventBus } from './utilities/EventBus';
 import { RedisManager } from './utilities/RedisManager';
 import { StateManager } from './utilities/StateManager';
+import { CompatServiceDefinition } from 'nice-grpc/lib/service-definitions';
+import { ConduitModule } from './classes/ConduitModule';
+import { Client } from 'nice-grpc';
 
 export default class ConduitGrpcSdk {
   private readonly serverUrl: string;
   private readonly _config: Config;
   private readonly _admin: Admin;
   private readonly _router: Router;
-  private readonly _modules: any = {};
+  private readonly _modules: { [key: string]: ConduitModule<any> } = {};
   private readonly _availableModules: any = {
     database: DatabaseProvider,
     storage: Storage,
@@ -32,6 +35,8 @@ export default class ConduitGrpcSdk {
     chat: Chat,
     forms: Forms,
   };
+
+  private _dynamicModules: { [key: string]: CompatServiceDefinition } = {};
   private _eventBus?: EventBus;
   private _stateManager?: StateManager;
   private lastSearch: number = Date.now();
@@ -84,7 +89,7 @@ export default class ConduitGrpcSdk {
 
   get database(): DatabaseProvider | null {
     if (this._modules['database']) {
-      return this._modules['database'];
+      return this._modules['database'] as DatabaseProvider;
     } else {
       console.warn('Database provider not up yet!');
       return null;
@@ -97,7 +102,7 @@ export default class ConduitGrpcSdk {
 
   get storage(): Storage | null {
     if (this._modules['storage']) {
-      return this._modules['storage'];
+      return this._modules['storage'] as Storage;
     } else {
       console.warn('Storage module not up yet!');
       return null;
@@ -106,7 +111,7 @@ export default class ConduitGrpcSdk {
 
   get forms(): Forms | null {
     if (this._modules['forms']) {
-      return this._modules['forms'];
+      return this._modules['forms'] as Forms;
     } else {
       console.warn('Forms module not up yet!');
       return null;
@@ -115,7 +120,7 @@ export default class ConduitGrpcSdk {
 
   get emailProvider(): Email | null {
     if (this._modules['email']) {
-      return this._modules['email'];
+      return this._modules['email'] as Email;
     } else {
       console.warn('Email provider not up yet!');
       return null;
@@ -124,7 +129,7 @@ export default class ConduitGrpcSdk {
 
   get pushNotifications(): PushNotifications | null {
     if (this._modules['pushNotifications']) {
-      return this._modules['pushNotifications'];
+      return this._modules['pushNotifications'] as PushNotifications;
     } else {
       console.warn('Push notifications module not up yet!');
       return null;
@@ -133,7 +138,7 @@ export default class ConduitGrpcSdk {
 
   get authentication(): Authentication | null {
     if (this._modules['authentication']) {
-      return this._modules['authentication'];
+      return this._modules['authentication'] as Authentication;
     } else {
       console.warn('Authentication module not up yet!');
       return null;
@@ -142,7 +147,7 @@ export default class ConduitGrpcSdk {
 
   get sms(): SMS | null {
     if (this._modules['sms']) {
-      return this._modules['sms'];
+      return this._modules['sms'] as SMS;
     } else {
       console.warn('SMS module not up yet!');
       return null;
@@ -151,7 +156,7 @@ export default class ConduitGrpcSdk {
 
   get chat(): Chat | null {
     if (this._modules['chat']) {
-      return this._modules['chat'];
+      return this._modules['chat'] as Chat;
     } else {
       console.warn('Chat module not up yet!');
       return null;
@@ -169,14 +174,10 @@ export default class ConduitGrpcSdk {
         }
       });
       modules.forEach((m: any) => {
-        if (!this._modules[m.moduleName] && this._availableModules[m.moduleName]) {
-          this._modules[m.moduleName] = new this._availableModules[m.moduleName](
-            this.name,
-            m.url,
-          );
-        } else if (this._availableModules[m.moduleName]) {
-          this._modules[m.moduleName]?.initializeClient();
+        if (this._modules[m.moduleName] && this._availableModules[m.moduleName]) {
+          this._modules[m.moduleName]?.openConnection();
         }
+        this.createModuleClient(m.moduleName, m.url);
       });
     });
   }
@@ -206,12 +207,7 @@ export default class ConduitGrpcSdk {
       .then((r) => {
         this.lastSearch = Date.now();
         r.forEach((m) => {
-          if (!this._modules[m.moduleName] && this._availableModules[m.moduleName]) {
-            this._modules[m.moduleName] = new this._availableModules[m.moduleName](
-              this.name,
-              m.url,
-            );
-          }
+          this.createModuleClient(m.moduleName, m.url);
         });
         return 'ok';
       })
@@ -220,6 +216,27 @@ export default class ConduitGrpcSdk {
           console.error(err);
         }
       });
+  }
+
+  createModuleClient(moduleName: string, moduleUrl: string) {
+    if (this._modules[moduleName] || (!this._availableModules[moduleName] && !this._dynamicModules[moduleName])) return;
+    if (this._availableModules[moduleName]) {
+      this._modules[moduleName] = new this._availableModules[moduleName](
+        moduleName,
+        moduleUrl,
+      );
+    } else if (this._dynamicModules[moduleName]) {
+      this._modules[moduleName] = new ConduitModule(moduleName, moduleUrl);
+      this._modules[moduleName].initializeClient(this._dynamicModules[moduleName]);
+    }
+  }
+
+  moduleClient(name: string, type: CompatServiceDefinition): void {
+    this._dynamicModules[name] = type;
+  }
+
+  getModule<T extends CompatServiceDefinition>(name: string): Client<T> | undefined {
+    if (this._modules[name]) return this._modules[name].client!;
   }
 
   isAvailable(moduleName: string) {
