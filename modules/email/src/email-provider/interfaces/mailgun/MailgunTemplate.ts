b7a5f6323c00027ab28829a510819165176ab84e
sdimitris <dimitrissoldatos2@gmail.com>
2022-03-09 11:48:13 +0200

    feat(email): interfaces for mailgun template

--- modules/email/src/email-provider/interfaces/mailgun/MailgunTemplate.ts
+++ modules/email/src/email-provider/interfaces/mailgun/MailgunTemplate.ts
@@ -1,3 +1,20 @@
 export interface MailgunTemplate  {
+    template: {
+        name: string;
+        description: string;
+        createdAt: any;
+        createdBy:any;
+        id: any;
+        version: {
+            tag: string;
+            template: string;
+            engine: string;
+            mjml: string;
+            createdAt: string;
+            comment: string;
+            active: boolean;
+            id: string;
+        }
+    }
     description?: string;
 }
