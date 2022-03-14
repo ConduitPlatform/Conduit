a51b98d25854d799e86c48e4b6404f1b51c2cc58
Konstantinos Feretos <konferetos@tutanota.com>
2022-03-14 14:59:57 +0200

    fix(storage): updateFile fixes chore(storage): clean up getFileUrl redirect check

--- modules/storage/src/handlers/file.ts
+++ modules/storage/src/handlers/file.ts
@@ -151,22 +151,22 @@ export class FileHandlers {
       }
       const newContainer = container ?? found.container;
 
-      const shouldRemove =
-        newName !== found.name ||
-        newContainer !== found.container ||
-        newFolder !== found.folder;
+      const isDataUpdate =
+        newName === found.name &&
+        newContainer === found.container &&
+        newFolder === found.folder;
 
       found.mimeType = mimeType ?? found.mimeType;
 
       if (newContainer !== found.container) {
-        let container = await _StorageContainer.getInstance().findOne({
+        const container = await _StorageContainer.getInstance().findOne({
           name: newContainer,
         });
         if (!container) {
           if (!config.allowContainerCreation) {
             throw new GrpcError(status.PERMISSION_DENIED, 'Container creation is not allowed!');
           }
-          let exists = await this.storageProvider.containerExists(newContainer);
+          const exists = await this.storageProvider.containerExists(newContainer);
           if (!exists) {
             await this.storageProvider.createContainer(newContainer);
           }
@@ -175,7 +175,7 @@ export class FileHandlers {
       }
 
       if (newFolder !== found.folder) {
-        let exists = await this.storageProvider
+        const exists = await this.storageProvider
           .container(newContainer)
           .folderExists(newFolder);
         if (!exists) {
@@ -187,21 +187,29 @@ export class FileHandlers {
         }
       }
 
-      if (shouldRemove) {
+      const exists = await File.getInstance().findOne({
+        name: newName,
+        container: newContainer,
+        folder: newFolder,
+      });
+      if (!isDataUpdate && exists) {
+        throw new GrpcError(status.ALREADY_EXISTS, 'File already exists');
+      }
+
+      await this.storageProvider
+        .container(newContainer)
+        .store((newFolder ?? '') + newName, fileData);
+      // calling delete after store call succeeds
+      if (!isDataUpdate) {
         await this.storageProvider
           .container(found.container)
           .delete((found.folder ?? '') + found.name);
       }
-      await this.storageProvider
-        .container(newContainer)
-        .store((newFolder ?? '') + newName, fileData);
 
       found.name = newName;
       found.folder = newFolder;
       found.container = newContainer;
-      const updatedFile = await File.getInstance().findByIdAndUpdate(found._id, found) as File;
-
-      return updatedFile;
+      return await File.getInstance().findByIdAndUpdate(found._id, found) as File;
     } catch (e) {
       throw new GrpcError(status.INTERNAL, e.message ?? 'Something went wrong!');
     }
@@ -243,7 +251,7 @@ export class FileHandlers {
         .container(found.container)
         .getSignedUrl((found.folder ?? '') + found.name);
 
-      if (!isNil(call.request.params.redirect) && (call.request.params.redirect === 'false' || !call.request.params.redirect)) {
+      if (!call.request.params.redirect) {
         return { result: url };
       }
       return { redirect: url };
