eb9e4290ee1e1a8ec7f9c0c651577a7a4f180984
Konstantinos Feretos <konferetos@tutanota.com>
2022-04-19 19:19:11 +0300

    chore(grpc-sdk): delete accidentally pushed file

--- libraries/grpc-sdk/src/modules/core/index.ts
+++
@@ -1,9 +0,0 @@
-import { ConduitModule } from '../../classes/ConduitModule';
-import { CoreDefinition } from '../../protoUtils/core';
-
-export class Core extends ConduitModule<typeof CoreDefinition> {
-  constructor(moduleName: string, url: string) {
-    super(moduleName, 'sms', url);
-    this.initializeClient(CoreDefinition);
-  }
-}
