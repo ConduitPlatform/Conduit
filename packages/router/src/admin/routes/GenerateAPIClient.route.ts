46d39b4da66dcde8c8b05329a4194531c7746684
Michael-Vol <mivolakis@gmail.com>
2022-05-04 18:01:33 +0300

    feat(database): add generation handler

--- packages/router/src/admin/routes/GenerateAPIClient.route.ts
+++ packages/router/src/admin/routes/GenerateAPIClient.route.ts
@@ -6,8 +6,13 @@ import {
   ConduitString,
   TYPE,
 } from '@conduitplatform/commons';
-import { ParsedRouterRequest, UnparsedRouterResponse } from '@conduitplatform/grpc-sdk';
+import { GrpcError } from '@conduitplatform/grpc-sdk';
+import { status } from '@grpc/grpc-js';
 import { ConduitDefaultRouter } from '../..';
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+import path from 'path';
+import url from 'url';
 
 export function generateAPIClient(router: ConduitDefaultRouter) {
   return new ConduitRoute(
@@ -24,6 +29,29 @@ export function generateAPIClient(router: ConduitDefaultRouter) {
     async (request: ConduitRouteParameters) => {
       let response: any[] = [];
       console.log(request);
+      const outputLanguage = request.params!.outputLanguage;
+      const outputPath = path.resolve(__dirname, 'dist/generated_openapi_client');
+      console.log(outputPath);
+
+      let { error, stdout } = await exec(
+        `openapi-generator generate -i http://localhost:${
+          process.env['PORT'] ?? '3000'
+        }/swagger.json -g ${outputLanguage} -o ${outputPath}`
+      );
+      if (error) {
+        throw new GrpcError(status.INTERNAL, error.message);
+      }
+      console.log(stdout);
+      const tarPath = path.resolve(__dirname, 'dist/openapi-generator-cli.tar.gz');
+      ({ error, stdout } = await exec(`tar -czf ${tarPath} -C ${outputPath} .`));
+      if (error) {
+        throw new GrpcError(status.INTERNAL, error.message);
+      }
+      console.log(stdout);
+      response.push({
+        generated: 'ok',
+        file: url.pathToFileURL(tarPath).href,
+      });
       return { result: response };
     }
   );
