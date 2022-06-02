6cf7070187a66f86f216f8bb824b9fc397b083a5
kkopanidis <kkopanidis@gmail.com>
2022-06-02 12:12:29 +0300

    fix(database): crud ops migration

--- modules/database/src/migrations/crudOperations.migration.ts
+++ modules/database/src/migrations/crudOperations.migration.ts
@@ -1,13 +1,11 @@
 import { DatabaseAdapter } from '../adapters/DatabaseAdapter';
 import { MongooseSchema } from '../adapters/mongoose-adapter/MongooseSchema';
 import { SequelizeSchema } from '../adapters/sequelize-adapter/SequelizeSchema';
-import { ConduitActiveSchema, ConduitSchema } from '@conduitplatform/grpc-sdk';
 
 export async function migrateCrudOperations(adapter: DatabaseAdapter<MongooseSchema | SequelizeSchema>) {
   const model = adapter.getSchemaModel('_DeclaredSchema').model;
   let cmsSchemas = await model
-    .findMany({ 'modelOptions.conduit.cms': { $exists: true } });
-  cmsSchemas = cmsSchemas.filter((schema: any) => typeof schema.modelOptions.conduit!.cms.crudOperations === 'boolean');
+    .findMany({ $or: [{ 'modelOptions.conduit.cms.crudOperations': true }, { 'modelOptions.conduit.cms.crudOperations': false }]});
   for (const schema of cmsSchemas) {
     const { crudOperations, authentication, enabled } = schema.modelOptions.conduit.cms;
     const cms = {
