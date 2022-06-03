06c8b0a959622eed15f4dd8ae24869f4dc452ade
Konstantinos Feretos <konferetos@tutanota.com>
2022-06-03 17:29:06 +0300

    chore(database): remove leftover util

--- modules/database/src/adapters/utils/collectionName.ts
+++
@@ -1,8 +0,0 @@
-export function generateUniqueCollectionName(collectionName: string, length = 6) {
-  const validCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let idSuffix = '';
-  for (let i = 0; i < length; i++) {
-    idSuffix += validCharacters.charAt(Math.floor(Math.random() * validCharacters.length));
-  }
-  return `${collectionName}_${idSuffix}`;
-}
