4d5370a1567e2ced6744437518fda3147602dd3a
Konstantinos Feretos <konferetos@tutanota.com>
2022-06-20 13:56:51 +0300

    fix(database): security client schema ownership migration

--- modules/database/src/migrations/securityClients.migration.ts
+++ modules/database/src/migrations/securityClients.migration.ts
@@ -1,12 +1,10 @@
 import { DatabaseAdapter } from '../adapters/DatabaseAdapter';
 import { MongooseSchema } from '../adapters/mongoose-adapter/MongooseSchema';
 import { SequelizeSchema } from '../adapters/sequelize-adapter/SequelizeSchema';
-import { migrateCrudOperations } from './crudOperations.migration';
-import { migrateSecurityClients } from './securityClients.migration';
 
-export async function runMigrations(
+export async function migrateSecurityClients(
   adapter: DatabaseAdapter<MongooseSchema | SequelizeSchema>,
 ) {
-  await migrateCrudOperations(adapter);
-  await migrateSecurityClients(adapter);
+  const model = adapter.getSchemaModel('_DeclaredSchema').model;
+  await model.updateMany({ name: 'Client' }, { ownerModule: 'router' }, true);
 }
