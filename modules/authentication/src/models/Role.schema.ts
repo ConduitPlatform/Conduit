cf6219509a8df44a01f7b3f607697229f2904233
kkopanidis <kkopanidis@gmail.com>
2022-07-12 21:39:01 +0300

    feat(authentication): add role and group schemas
    WIP

---
+++ modules/authentication/src/models/Role.schema.ts
@@ -0,0 +1,81 @@
+import { ConduitActiveSchema, DatabaseProvider, TYPE } from '@conduitplatform/grpc-sdk';
+import { Group } from './Group.schema';
+
+const schema = {
+  _id: TYPE.ObjectId,
+  // do not add unique again, since this will fail due to emails being null
+  name: {
+    type: TYPE.String,
+    required: true,
+  },
+  group: {
+    type: TYPE.Relation,
+    model: 'Group',
+    required: true,
+  },
+  isDefault: {
+    type: TYPE.Boolean,
+    default: false,
+  },
+  permissions: {
+    canInvite: {
+      type: TYPE.Boolean,
+      default: false,
+    },
+    canRemove: {
+      type: TYPE.Boolean,
+      default: false,
+    },
+    canDeleteGroup: {
+      type: TYPE.Boolean,
+      default: false,
+    },
+    canCreateGroup: {
+      type: TYPE.Boolean,
+      default: false,
+    },
+  },
+  createdAt: TYPE.Date,
+  updatedAt: TYPE.Date,
+};
+const schemaOptions = {
+  timestamps: true,
+  conduit: {
+    permissions: {
+      extendable: true,
+      canCreate: false,
+      canModify: 'ExtensionOnly',
+      canDelete: false,
+    },
+  },
+} as const;
+const collectionName = undefined;
+
+export class Role extends ConduitActiveSchema<Role> {
+  private static _instance: Role;
+  _id: string;
+  name: string;
+  group: string | Group;
+  isDefault: boolean;
+  permissions: {
+    canInvite: boolean;
+    canRemove: boolean;
+    canDeleteGroup: boolean;
+    canCreateGroup: boolean;
+  };
+  createdAt: Date;
+  updatedAt: Date;
+
+  constructor(database: DatabaseProvider) {
+    super(database, Role.name, schema, schemaOptions, collectionName);
+  }
+
+  static getInstance(database?: DatabaseProvider) {
+    if (Role._instance) return Role._instance;
+    if (!database) {
+      throw new Error('No database instance provided!');
+    }
+    Role._instance = new Role(database);
+    return Role._instance;
+  }
+}
