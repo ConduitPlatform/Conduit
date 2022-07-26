d420b945be534f31c370796efcb89800010f21b4
Michael-Vol <mivolakis@gmail.com>
2022-07-26 15:30:34 +0300

    fix(core): fix get config routes not working

--- packages/core/src/config-manager/admin/routes/RegisterConfigRoute.ts
+++ packages/core/src/config-manager/admin/routes/RegisterConfigRoute.ts
@@ -1,11 +1,15 @@
 import ConduitGrpcSdk, {
+  ConduitError,
   ConduitRouteActions,
   ConduitRouteParameters,
 } from '@conduitplatform/grpc-sdk';
 import { ConduitRoute, ConduitRouteReturnDefinition } from '@conduitplatform/hermes';
+import { ConduitCommons } from '@conduitplatform/commons';
+import { update } from 'lodash';
 
 export function registerConfigRoute(
   grpcSdk: ConduitGrpcSdk,
+  conduit: ConduitCommons,
   moduleName: string,
   configSchema: any,
   routeAction: ConduitRouteActions.GET | ConduitRouteActions.PATCH,
@@ -27,17 +31,48 @@ export function registerConfigRoute(
       },
     ),
     async (params: ConduitRouteParameters) => {
-      const newConfig = params.params!.config;
-      const updatedConfig = JSON.parse(
-        // @ts-ignore
-        (
-          await grpcSdk
-            .getModule<any>(moduleName)!
-            // @ts-ignore
-            .setConfig({ newConfig: JSON.stringify(newConfig) })
-        ).updatedConfig,
-      );
-      return { result: { config: updatedConfig } };
+      if (routeAction === ConduitRouteActions.GET) {
+        let finalConfig;
+        finalConfig = await grpcSdk.state!.getKey(`moduleConfigs.${moduleName}`);
+        if (!finalConfig) {
+          finalConfig = {};
+        } else {
+          finalConfig = JSON.parse(finalConfig);
+        }
+        return { result: { config: finalConfig } };
+      } else {
+        const newConfig = params.params!.config;
+        let updatedConfig;
+        switch (moduleName) {
+          case 'core':
+            updatedConfig = await conduit
+              .getCore()
+              .setConfig(newConfig)
+              .catch(e => {
+                throw new ConduitError(e.name, e.status ?? 500, e.message);
+              });
+            break;
+          case 'admin':
+            updatedConfig = await conduit
+              .getAdmin()
+              .setConfig(newConfig)
+              .catch(e => {
+                throw new ConduitError(e.name, e.status ?? 500, e.message);
+              });
+            break;
+          default:
+            updatedConfig = JSON.parse(
+              // @ts-ignore
+              (
+                await grpcSdk
+                  .getModule<any>(moduleName)!
+                  // @ts-ignore
+                  .setConfig({ newConfig: JSON.stringify(newConfig) })
+              ).updatedConfig,
+            );
+        }
+        return { result: { config: updatedConfig } };
+      }
     },
   );
 }
