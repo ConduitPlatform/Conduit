f9183c9002e809305aca95e8a4e34b2cde10d732
Konstantinos Feretos <konferetos@tutanota.com>
2022-07-26 17:41:05 +0300

    fix(grpc-sdk): avoid subscribing to config updates for non-configurable modules

--- libraries/grpc-sdk/src/classes/ManagedModule.ts
+++ libraries/grpc-sdk/src/classes/ManagedModule.ts
@@ -137,6 +137,7 @@ export abstract class ManagedModule<T> extends ConduitServiceModule {
 
   /** Used to update the module's configuration on initial Redis/DB reconciliation. */
   async handleConfigSyncUpdate() {
+    if (!this.config) return;
     this.grpcSdk.bus!.subscribe(`${this.name}:config:update`, async message => {
       ConfigController.getInstance().config = await this.preConfig(JSON.parse(message));
       await this.onConfig();
