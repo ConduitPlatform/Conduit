9992bc124f363041fdee224d00fb32fe07f92853
Konstantinos Feretos <konferetos@tutanota.com>
2022-08-04 12:16:35 +0300

    chore(core): remove legacy ping routes

--- packages/core/src/routes/index.ts
+++ packages/core/src/routes/index.ts
@@ -1,12 +1,9 @@
 import ConduitGrpcSdk, {
   ConduitRouteActions as Actions,
   ConduitRouteReturnDefinition,
-  GrpcError,
   GrpcServer as ConduitGrpcServer,
   RoutingManager,
 } from '@conduitplatform/grpc-sdk';
-import { Core } from '../Core';
-import { Status } from '@grpc/grpc-js/build/src/constants';
 
 export class HttpServer {
   private _routingManager: RoutingManager;
@@ -18,34 +15,9 @@ export class HttpServer {
   }
 
   private registerRoutes() {
-    this._routingManager.route(
-      {
-        path: '/',
-        action: Actions.GET,
-      },
-      new ConduitRouteReturnDefinition('HelloResult', 'String'),
-      async () => {
-        return 'Hello there!';
-      },
-    );
-
-    this._routingManager.route(
-      {
-        path: '/health',
-        action: Actions.GET,
-        queryParams: {
-          shouldCheck: 'String',
-        },
-      },
-      new ConduitRouteReturnDefinition('HealthResult', 'String'),
-      async () => {
-        if (Core.getInstance().initialized) {
-          return 'Conduit is online!';
-        } else {
-          throw new GrpcError(Status.FAILED_PRECONDITION, 'Conduit is not active yet!');
-        }
-      },
-    );
+    // this._routingManager.route(
+    //   ...
+    // );
     this._routingManager.registerRoutes().catch(() => {});
   }
 }
