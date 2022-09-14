867316367e1661dbb6502699d125439aacb3d341
chris <christinapdgn@gmail.com>
2022-09-14 15:22:52 +0300

    chore(admin): add admin route descriptions

--- packages/admin/src/routes/DeleteAdminUser.route.ts
+++ packages/admin/src/routes/DeleteAdminUser.route.ts
@@ -13,6 +13,7 @@ export function deleteAdminUserRoute() {
     {
       path: '/admins/:id',
       action: ConduitRouteActions.DELETE,
+      description: `Deletes an admin user given a specific id.`,
       urlParams: {
         id: ConduitString.Required,
       },
--- packages/admin/src/routes/GetAdminUsers.route.ts
+++ packages/admin/src/routes/GetAdminUsers.route.ts
@@ -13,6 +13,7 @@ export function getAdminUsersRoute() {
     {
       path: '/admins',
       action: ConduitRouteActions.GET,
+      description: `Returns all admin users and their total number.`,
       queryParams: {
         skip: ConduitNumber.Optional,
         limit: ConduitNumber.Optional,
