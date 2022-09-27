575d58f3cd60f61fd8a0ea8b243e9c171a63548f
Konstantinos Kopanidis <kkopanidis@users.noreply.github.com>
2022-09-27 18:33:27 +0300

    refactor(authentication): remove group/role changes

--- modules/authentication/src/models/index.ts
+++ modules/authentication/src/models/index.ts
@@ -3,6 +3,4 @@ export * from './RefreshToken.schema';
 export * from './Token.schema';
 export * from './User.schema';
 export * from './Service.schema';
-export * from './Group.schema';
-// export * from './Role.schema';
 export * from './TwoFactorSecret.schema';
