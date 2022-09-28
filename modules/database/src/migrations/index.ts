8a4ecf845811cae6c3bed5b7f88afe336f368ce3
Konstantinos Feretos <konferetos@tutanota.com>
2022-09-28 11:42:55 +0300

    fix(database): migrate custom endpoints with missing selectedSchema id

--- modules/database/src/migrations/index.ts
+++ modules/database/src/migrations/index.ts
@@ -3,10 +3,12 @@ import { MongooseSchema } from '../adapters/mongoose-adapter/MongooseSchema';
 import { SequelizeSchema } from '../adapters/sequelize-adapter/SequelizeSchema';
 import { migrateCrudOperations } from './crudOperations.migration';
 import { migrateSecurityClients } from './securityClients.migration';
+import { migrateCustomEndpoints } from './customEndpoints.migration';
 
 export async function runMigrations(
   adapter: DatabaseAdapter<MongooseSchema | SequelizeSchema>,
 ) {
   await migrateCrudOperations(adapter);
   await migrateSecurityClients(adapter);
+  await migrateCustomEndpoints(adapter);
 }
