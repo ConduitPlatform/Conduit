b9da46811e87826e7e30085c9abfa3fca606bda0
Konstantinos Feretos <konferetos@tutanota.com>
2022-10-10 16:02:22 +0300

    fix(admin): generated config values reset on partial config update (#375)
    fix(admin): setConfig() not deep merging config objects

--- packages/admin/src/utils/config.ts
+++ packages/admin/src/utils/config.ts
@@ -2,9 +2,7 @@ import { Config as ConfigSchema } from '../config';
 import crypto from 'crypto';
 import { isNil } from 'lodash';
 
-export async function generateConfigDefaults(
-  config: ConfigSchema,
-): Promise<ConfigSchema> {
+export async function generateConfigDefaults(config: ConfigSchema) {
   const tokenSecretConfig = config.auth.tokenSecret;
   if (tokenSecretConfig === '' || isNil(tokenSecretConfig)) {
     config.auth.tokenSecret = crypto.randomBytes(64).toString('base64');
@@ -14,5 +12,4 @@ export async function generateConfigDefaults(
       process.env.__DEFAULT_HOST_URL ??
       `http://localhost:${process.env['ADMIN_HTTP_PORT'] ?? '3030'}`;
   }
-  return config;
 }
