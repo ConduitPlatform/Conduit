17efde0266d5369ab7013a09731219766a124fde
roula <swtiria.stefa@gmail.com>
2022-12-09 15:22:59 +0200

    chore:  schema owner from user  input

--- scripts/migrateV10_V15/index.js
+++ scripts/migrateV10_V15/index.js
@@ -1,8 +1,8 @@
-const  migrateV10_V11  = require('../migrateV10_V15/migrationFiles/migrateV10_V11');
-const  migrateV11_V12  = require('../migrateV10_V15/migrationFiles/migrateV11_V12');
-const  migrateV13_V14  = require('../migrateV10_V15/migrationFiles/migrateV13_V14');
-const  migrateV14_V15  = require('../migrateV10_V15/migrationFiles/migrateV14_V15');
 const migrateOwnerModule = require('../migrateV10_V15/migrationFiles/schemaOwnerModule');
+const migrateV10_V11 = require('../migrateV10_V15/migrationFiles/migrateV10_V11');
+const migrateV11_V12 = require('../migrateV10_V15/migrationFiles/migrateV11_V12');
+const migrateV13_V14 = require('../migrateV10_V15/migrationFiles/migrateV13_V14');
+const migrateV14_V15 = require('../migrateV10_V15/migrationFiles/migrateV14_V15');
 const { MongoConnection } = require('./mongoConnection');
 
 const migrate = async () => {
@@ -11,17 +11,16 @@ const migrate = async () => {
     await migrateV10_V15();
     console.log('Migration completed successfully');
     process.exit(0);
-  }
-  catch (e) {
+  } catch (e) {
     console.log(e);
   }
 };
 const migrateV10_V15 = async () => {
+  await migrateOwnerModule();
   await migrateV10_V11();
   await migrateV11_V12();
   await migrateV13_V14();
   await migrateV14_V15();
-  await migrateOwnerModule();
 };
 
 migrate().then(r => console.log(r)).catch(e => console.log(e));
--- scripts/migrateV10_V15/migrationFiles/schemaOwnerModule.js
+++ scripts/migrateV10_V15/migrationFiles/schemaOwnerModule.js
@@ -1,23 +1,48 @@
 const db = require('../mongoConnection');
+const readline = require('readline');
 
 const migrateSchemaOwnerModule = async () => {
-  const schemaNames = [
-    'AccessToken',
-    'RefreshToken',
-    'Token',
-    'TwoFactorSecret',
-    'Config',
-    'User',
-  ];
 
-  const documents = db.collection('_declaredSchemas');
-  await documents.updateMany({ name: { $in: schemaNames } }, { $set: { ownerModule: 'authentication' } });
+  const dictionary = {
+    authentication: ['AccessToken', 'RefreshToken', 'Token', 'User'],
+    database: ['SchemaDefinitions', 'CustomEndpoints'],
+    chat: ['ChatMessage', 'ChatRoom'],
+    forms: ['Forms', 'FormReplies'],
+    email: ['EmailTemplate'],
+    pushNotifications: ['NotificationToken'],
+    storage: ['File', '_StorageContainer', '_StorageFolder'],
+  };
 
 
-}
+  const documents = db.collection('_declaredschemas');
+  const schemas = await documents.find({}).toArray();
+  for (const schema of schemas) {
+    const { name } = schema;
+    const ownerModule = Object.keys(dictionary).find((key) => dictionary[key].includes(name));
+    if (ownerModule) {
+      await documents.updateOne({ _id: schema._id }, { $set: { ownerModule } });
+    } else {
+      let owner;
+      console.log('No owner module found for schema: ', name);
+      const inputOwnerModule = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+
+      inputOwnerModule.question('Please enter the owner module: ', (userInput) => {
+        owner = userInput;
+        inputOwnerModule.close();
+      });
+      await documents.updateOne({ _id: schema._id }, { $set: { ownerModule: owner } });
+    }
+
+  }
+
+
+};
 
 const migrateOwnerModule = async () => {
   await migrateSchemaOwnerModule();
-}
+};
 
 module.exports = migrateOwnerModule;
