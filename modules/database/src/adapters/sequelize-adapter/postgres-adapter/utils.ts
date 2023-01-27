3e745609ef728e31623359fe93c2dc0538215e61
Konstantinos Kopanidis <kkopanidis@users.noreply.github.com>
2023-01-27 20:52:35 +0200

    fix(database): queries for related objects

--- modules/database/src/adapters/sequelize-adapter/postgres-adapter/utils.ts
+++ modules/database/src/adapters/sequelize-adapter/postgres-adapter/utils.ts
@@ -3,15 +3,20 @@ import { isArray, isBoolean, isString } from 'lodash';
 import { Indexable } from '@conduitplatform/grpc-sdk';
 import { ParsedQuery } from '../../../interfaces';
 
-function arrayHandler(value: any, relations?: Indexable) {
+function arrayHandler(value: any, relations: Indexable, relationsDirectory: string[]) {
   const newArray = [];
   for (const val of value) {
-    newArray.push(parseQuery(val, relations));
+    newArray.push(parseQuery(val, relations, relationsDirectory));
   }
   return newArray;
 }
 
-function matchOperation(operator: string, value: any, relations?: Indexable) {
+function matchOperation(
+  operator: string,
+  value: any,
+  relations: Indexable,
+  relationsDirectory: string[],
+) {
   switch (operator) {
     case '$eq':
       return { [Op.eq]: value };
@@ -38,32 +43,36 @@ function matchOperation(operator: string, value: any, relations?: Indexable) {
     case '$lte':
       return { [Op.lte]: value };
     case '$in':
-      return { [Op.in]: arrayHandler(value, relations) };
+      return { [Op.in]: arrayHandler(value, relations, relationsDirectory) };
     case '$or':
-      return arrayHandler(value, relations);
+      return arrayHandler(value, relations, relationsDirectory);
     case '$and':
-      return arrayHandler(value, relations);
+      return arrayHandler(value, relations, relationsDirectory);
     case '$nin':
-      return { [Op.notIn]: arrayHandler(value, relations) };
+      return { [Op.notIn]: arrayHandler(value, relations, relationsDirectory) };
     default:
       return value;
   }
 }
 
-export function parseQuery(query: ParsedQuery, relations?: Indexable) {
+export function parseQuery(
+  query: ParsedQuery,
+  relations: Indexable,
+  relationsDirectory: string[],
+) {
   const parsed: Indexable = isArray(query) ? [] : {};
   if (isString(query) || isBoolean(query)) return query;
   for (const key in query) {
     if (key === '$or') {
       Object.assign(parsed, {
         [Op.or]: query[key].map((operation: ParsedQuery) =>
-          parseQuery(operation, relations),
+          parseQuery(operation, relations, relationsDirectory),
         ),
       });
     } else if (key === '$and') {
       Object.assign(parsed, {
         [Op.and]: query[key].map((operation: ParsedQuery) =>
-          parseQuery(operation, relations),
+          parseQuery(operation, relations, relationsDirectory),
         ),
       });
     } else if (key === '$regex') {
@@ -71,15 +80,18 @@ export function parseQuery(query: ParsedQuery, relations?: Indexable) {
     } else if (key === '$options') {
       continue;
     } else {
-      const subQuery = parseQuery(query[key], relations);
+      const subQuery = parseQuery(query[key], relations, relationsDirectory);
       if (subQuery === undefined) continue;
-      const matched = matchOperation(key, subQuery, relations);
+      const matched = matchOperation(key, subQuery, relations, relationsDirectory);
       if (key.indexOf('$') !== -1) {
         Object.assign(parsed, matched);
         continue;
       }
       const relationKey = key.indexOf('.') !== -1 ? key.split('.')[0] : key;
       if (relations && relations[relationKey]) {
+        if (relationsDirectory.indexOf(key) === -1) {
+          relationsDirectory.push(key);
+        }
         parsed[`$${key}${key.indexOf('.') !== -1 ? '' : '._id'}$`] = matched;
       } else {
         parsed[key] = matched;
