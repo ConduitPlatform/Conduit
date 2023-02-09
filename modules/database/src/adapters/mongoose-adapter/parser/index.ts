a6989daccbaef6abc8e2985c015e0869e845532f
Konstantinos Feretos <konferetos@tutanota.com>
2023-02-09 15:29:13 +0200

    chore(database): simplify mongoose query like/ilike parsing

--- modules/database/src/adapters/mongoose-adapter/parser/index.ts
+++ modules/database/src/adapters/mongoose-adapter/parser/index.ts
@@ -1,58 +1,23 @@
-import { ParsedQuery } from '../../../interfaces';
-import { isArray, isBoolean, isNumber, isString } from 'lodash';
 import { Indexable } from '@conduitplatform/grpc-sdk';
+import { ParsedQuery } from '../../../interfaces';
 
 export function parseQuery(query: ParsedQuery): ParsedQuery {
-  return _parseQuery(query) as ParsedQuery;
-}
-
-export function _parseQuery(query?: ParsedQuery | null): ParsedQuery | null | undefined {
-  if (query === undefined) return undefined;
-  if (query === null) return null;
-  const parsed: Indexable = isArray(query) ? [] : {};
-  if (isString(query) || isBoolean(query) || isNumber(query)) return query;
-  for (const key in query) {
-    if (key === '$or') {
-      Object.assign(parsed, {
-        $or: query[key].map((operation: ParsedQuery) => {
-          return parseQuery(operation);
-        }),
-      });
-    } else if (key === '$and') {
-      Object.assign(parsed, {
-        $and: query[key].map((operation: ParsedQuery) => {
-          return parseQuery(operation);
-        }),
-      });
-    } else {
-      if (!!query[key] && typeof query[key] === 'object' && !Array.isArray(query[key])) {
-        const likeCandidates = Object.keys(query[key]);
-        if (likeCandidates.includes('$like')) {
-          Object.assign(parsed, {
-            [key]: { $regex: query[key].$like.replace(/%/g, '.*') },
-          });
-          continue;
-        }
-        if (likeCandidates.includes('$ilike')) {
-          Object.assign(parsed, {
-            [key]: {
-              $regex: query[key].$ilike.replace(/%/g, '.*'),
-              $options: 'i',
-            },
-          });
-          continue;
-        }
-      }
-      if (query[key] === undefined) {
-        Object.assign(parsed, {
-          [key]: undefined,
-        });
+  if (Array.isArray(query)) {
+    return query.map(item => parseQuery(item));
+  } else if (typeof query === 'object' && query !== null) {
+    const parsedQuery: Indexable = {};
+    Object.keys(query).forEach(key => {
+      if (key === '$like') {
+        parsedQuery.$regex = query.$like.replace(/%/g, '.*');
+      } else if (key === '$ilike') {
+        parsedQuery.$regex = query.$ilike.replace(/%/g, '.*');
+        parsedQuery.$options = 'i';
       } else {
-        Object.assign(parsed, {
-          [key]: parseQuery(query[key]),
-        });
+        parsedQuery[key] = parseQuery(query[key]);
       }
-    }
+    });
+    return parsedQuery;
+  } else {
+    return query;
   }
-  return parsed;
 }
