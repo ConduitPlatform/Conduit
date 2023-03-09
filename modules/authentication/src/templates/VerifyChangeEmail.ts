a8cae8ebb1d0ecde7bba202544528936a03c8353
Konstantinos Kopanidis <kkopanidis@users.noreply.github.com>
2023-03-09 15:20:02 +0200

    refactor(authentication,chat): add raw link in templates since some clients do not render the anchor (#551)

--- modules/authentication/src/templates/VerifyChangeEmail.ts
+++ modules/authentication/src/templates/VerifyChangeEmail.ts
@@ -1,6 +1,6 @@
 export const VerifyChangeEmailTemplate = {
   name: 'ChangeEmailVerification',
   subject: 'Verify your new email',
-  body: 'Click <a href="{{link}}">here</a> to verify your new email',
+  body: '<p>Click <a href="{{link}}">here</a> to verify your new email</p><p>or use this link: {{link}}</p>',
   variables: ['link'],
 };
