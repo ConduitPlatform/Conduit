289b74a9396d341e32561f8ca04907f41429feec
Konstantinos Feretos <konferetos@tutanota.com>
2023-03-30 15:28:40 +0300

    feat(grpc-sdk): url remap object config (#577)

---
+++ libraries/grpc-sdk/src/utilities/env.ts
@@ -0,0 +1,29 @@
+import fs from 'fs-extra';
+
+export function getJsonEnv<JsonFormat extends object>(
+  envName: string,
+  envValue?: string,
+  handleString?: (value: string) => object,
+): JsonFormat | undefined {
+  envValue ??= process.env[envName];
+  if (!envValue) return undefined;
+  let jsonConfig: object;
+  if (envValue.startsWith('{')) {
+    try {
+      jsonConfig = JSON.parse(envValue);
+    } catch (e) {
+      throw new Error(`Invalid JSON in ${envName}`);
+    }
+  } else {
+    try {
+      jsonConfig = JSON.parse(fs.readFileSync(envValue).toString().trimEnd());
+    } catch (e) {
+      if (handleString) {
+        jsonConfig = handleString(envValue);
+      } else {
+        throw new Error(`Invalid JSON in ${envName}`);
+      }
+    }
+  }
+  return jsonConfig as JsonFormat;
+}
