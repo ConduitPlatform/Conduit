30abd87a703efc37bcb67eb865e818e1ad96ed86
Konstantinos Kopanidis <kkopanidis@users.noreply.github.com>
2023-05-06 17:13:02 +0300

    feat(database): add extra field validation to disallow relations in arrays of embedded objects

--- modules/database/src/adapters/utils/validateFieldConstraints.ts
+++ modules/database/src/adapters/utils/validateFieldConstraints.ts
@@ -6,11 +6,16 @@ import { isObject } from 'lodash';
  * Validates schema field constraints.
  * 'unique' requires 'required'
  */
-export function validateFieldConstraints(schema: ConduitDatabaseSchema) {
-  fieldsValidator(schema.name, schema.compiledFields);
+export function validateFieldConstraints(schema: ConduitDatabaseSchema, db: string) {
+  fieldsValidator(schema.name, schema.compiledFields, db);
 }
 
-export function fieldsValidator(schemaName: string, schemaFields: ConduitModel) {
+export function fieldsValidator(
+  schemaName: string,
+  schemaFields: ConduitModel,
+  db: string,
+  blockRelations = false,
+) {
   Object.keys(schemaFields).forEach(f => {
     if (/(?<=[a-zA-Z0-9])_(?=[a-zA-Z0-9])|(?<=[a-zA-Z0-9])_$/g.test(f)) {
       throw new ConduitError(
@@ -48,9 +53,24 @@ export function fieldsValidator(schemaName: string, schemaFields: ConduitModel) 
               `Schema '${schemaName}' array field '${f}' has invalid format (array should contain a single type).`,
             );
           }
-          fieldsValidator(schemaName, target.type[0] as ConduitModel);
+          if (
+            (target.type[0] &&
+              typeof target.type[0] === 'object' &&
+              target.type[0].hasOwnProperty('type') &&
+              target.type[0].type !== 'Relation') ||
+            (target.type[0] && typeof target.type[0] === 'object')
+          ) {
+            fieldsValidator(schemaName, target.type[0] as ConduitModel, db, db === 'sql');
+          } else {
+            fieldsValidator(
+              schemaName,
+              target.type[0] as ConduitModel,
+              db,
+              blockRelations,
+            );
+          }
         } else {
-          fieldsValidator(schemaName, target.type as ConduitModel);
+          fieldsValidator(schemaName, target.type as ConduitModel, db, blockRelations);
         }
       } else if (!target.hasOwnProperty('type') && isObject(target)) {
         if (Array.isArray(target)) {
@@ -61,10 +81,30 @@ export function fieldsValidator(schemaName: string, schemaFields: ConduitModel) 
               `Schema '${schemaName}' array field '${f}' has invalid format (array should contain a single type).`,
             );
           }
-          fieldsValidator(schemaName, target[0] as ConduitModel);
+          if (
+            (target[0] &&
+              typeof target[0] === 'object' &&
+              target[0].hasOwnProperty('type') &&
+              target[0].type !== 'Relation') ||
+            (target[0] && typeof target[0] === 'object')
+          ) {
+            fieldsValidator(schemaName, target[0] as ConduitModel, db, db === 'sql');
+          } else {
+            fieldsValidator(schemaName, target[0] as ConduitModel, db, blockRelations);
+          }
         } else {
-          fieldsValidator(schemaName, target as ConduitModel);
+          fieldsValidator(schemaName, target as ConduitModel, db, blockRelations);
         }
+      } else if (
+        target.hasOwnProperty('type') &&
+        target.type === 'Relation' &&
+        blockRelations
+      ) {
+        throw new ConduitError(
+          'INVALID_ARGUMENTS',
+          400,
+          `Schema '${schemaName}' violates field '${f}' constraint (relations not allowed in embedded objects).`,
+        );
       }
     }
   });
