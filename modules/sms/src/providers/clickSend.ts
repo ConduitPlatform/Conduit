66ec17f9160f5cffeb5d0421ab294480341303f0
Christina Papadogianni <59121443+ChrisPdgn@users.noreply.github.com>
2023-06-22 14:56:00 +0300

    fix(authentication,sms): hash sms phone numbers & fix bugs in authenticate() (#644)

--- modules/sms/src/providers/clickSend.ts
+++ modules/sms/src/providers/clickSend.ts
@@ -2,6 +2,7 @@ import { ISmsProvider } from '../interfaces/ISmsProvider';
 import ConduitGrpcSdk from '@conduitplatform/grpc-sdk';
 import { SMSApi, SmsMessage, SmsMessageCollection } from 'clicksend';
 import { generateToken } from '../utils';
+import * as bcrypt from 'bcrypt';
 export class clickSendProvider implements ISmsProvider {
   private readonly username: string;
   private readonly clicksendApiKey: string;
@@ -46,16 +47,17 @@ export class clickSendProvider implements ISmsProvider {
     };
     try {
       await this.client.smsSendPost(smsMessageCollection);
-      await this.grpcSdk.state!.setKey(phoneNumber, otp, 60000);
-      return phoneNumber;
+      const phoneNumberHash = await bcrypt.hash(phoneNumber, 11);
+      await this.grpcSdk.state!.setKey(phoneNumberHash, otp, 60000);
+      return phoneNumberHash;
     } catch (error) {
       ConduitGrpcSdk.Logger.error(error as Error);
       return 'could not send verification code';
     }
   }
 
-  async verify(phoneNumber: string, otp: string): Promise<boolean> {
-    const otpCode: string | null = await this.grpcSdk.state!.getKey(phoneNumber);
+  async verify(phoneNumberHash: string, otp: string): Promise<boolean> {
+    const otpCode: string | null = await this.grpcSdk.state!.getKey(phoneNumberHash);
     if (otpCode === null) {
       return Promise.reject(false);
     }
