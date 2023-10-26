c8301eb2d25d203f7439a1659582a7d50c1bae3a
Konstantinos Feretos <konferetos@tutanota.com>
2023-10-26 15:40:23 +0300

    fix: ts-proto snake_case to camelCase

--- modules/functions/build.sh
+++ modules/functions/build.sh
@@ -11,7 +11,8 @@ protoc \
   --plugin=../../node_modules/.bin/protoc-gen-ts_proto\
   --ts_proto_opt=esModuleInterop=true \
   --ts_proto_out=./ \
-  --ts_proto_opt=outputServices=generic-definitions,useExactTypes=false\
+  --ts_proto_opt=outputServices=generic-definitions,useExactTypes=false \
+  --ts_proto_opt=snakeToCamel=false \
   ./*.proto
 
 echo "Cleaning up folders"
