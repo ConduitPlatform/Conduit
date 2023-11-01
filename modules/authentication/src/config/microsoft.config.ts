304b9de3e6658097092c8ea9b75218efc190218c
Konstantinos Feretos <konferetos@tutanota.com>
2023-11-01 15:38:05 +0200

    chore(authentication): microsoft brand case

--- modules/authentication/src/config/microsoft.config.ts
+++ modules/authentication/src/config/microsoft.config.ts
@@ -4,12 +4,12 @@ export default {
   microsoft: {
     ...oauth2Schema,
     accountLinking: {
-      doc: 'When enabled, if a new microsoft user matches with an existing email on the database, they will be enriched with microsoft details',
+      doc: 'When enabled, if a new Microsoft user matches with an existing email on the database, they will be enriched with Microsoft details',
       format: 'Boolean',
       default: true,
     },
     tenantId: {
-      doc: 'The tenant id for the microsoft app. Used ONLY when app is not multi-tenant.',
+      doc: 'The tenant id for the Microsoft app. Used ONLY when app is not multi-tenant.',
       format: 'String',
       default: '',
     },
