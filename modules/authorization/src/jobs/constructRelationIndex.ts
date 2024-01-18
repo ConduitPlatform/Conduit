3ccd981259ae1f572b0240c97e4c4bfa9d9965b3
Konstantinos Feretos <konferetos@tutanota.com>
2024-01-18 15:57:25 +0200

    fix(authorization): constructRelationIndex not reusing grpc-sdk

--- modules/authorization/src/jobs/constructRelationIndex.ts
+++ modules/authorization/src/jobs/constructRelationIndex.ts
@@ -3,21 +3,25 @@ import ConduitGrpcSdk from '@conduitplatform/grpc-sdk';
 import { ActorIndex, ObjectIndex, ResourceDefinition } from '../models';
 import { IndexController } from '../controllers';
 
+let grpcSdk: ConduitGrpcSdk | undefined = undefined;
+
 type ConstructRelationIndexWorkerData = {
   relation: { subject: string; relation: string; object: string };
 };
 
 module.exports = async (job: SandboxedJob<ConstructRelationIndexWorkerData>) => {
   const { relation } = job.data;
-  if (!process.env.CONDUIT_SERVER) throw new Error('No serverUrl provided!');
-  const grpcSdk = new ConduitGrpcSdk(process.env.CONDUIT_SERVER, 'authorization', false);
-  await grpcSdk.initialize();
-  await grpcSdk.initializeEventBus();
-  await grpcSdk.waitForExistence('database');
-  ObjectIndex.getInstance(grpcSdk.database!);
-  ActorIndex.getInstance(grpcSdk.database!);
-  ResourceDefinition.getInstance(grpcSdk.database!);
-  IndexController.getInstance(grpcSdk);
+  if (!grpcSdk) {
+    if (!process.env.CONDUIT_SERVER) throw new Error('No serverUrl provided!');
+    grpcSdk = new ConduitGrpcSdk(process.env.CONDUIT_SERVER, 'authorization', false);
+    await grpcSdk.initialize();
+    await grpcSdk.initializeEventBus();
+    await grpcSdk.waitForExistence('database');
+    ObjectIndex.getInstance(grpcSdk.database!);
+    ActorIndex.getInstance(grpcSdk.database!);
+    ResourceDefinition.getInstance(grpcSdk.database!);
+    IndexController.getInstance(grpcSdk);
+  }
   await IndexController.getInstance().constructRelationIndex(
     relation.subject,
     relation.relation,
