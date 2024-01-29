e53162da749a14ccb59eb9014241eeab3b37afdc
Konstantinos Kopanidis <kkopanidis@users.noreply.github.com>
2024-01-29 20:18:25 +0200

    fix(forms): form schema types

--- modules/forms/src/models/Forms.schema.ts
+++ modules/forms/src/models/Forms.schema.ts
@@ -44,14 +44,14 @@ const collectionName = undefined;
 
 export class Forms extends ConduitActiveSchema<Forms> {
   private static _instance: Forms;
-  _id!: string;
+  _id: string;
   //todo rename
   declare name: string;
   //todo rename
-  declare fields: any;
-  forwardTo!: string;
-  emailField!: string;
-  enabled!: boolean;
+  declare fields: { [key: string]: NonNullable<unknown> };
+  forwardTo: string;
+  emailField: string;
+  enabled: boolean;
   createdAt: Date;
   updatedAt: Date;
 
