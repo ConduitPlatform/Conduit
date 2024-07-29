3bfc0ab5e5abe7c5c8a586aaa2dd1f620c79a5bc
chris <christinapdgn@gmail.com>
2024-07-29 14:09:27 +0300

    Revert "refactor: check if name is uuid"
    This reverts commit fadb026511a0a3508106b40111f4e51b28dbeb87.

--- modules/storage/src/utils/index.ts
+++ modules/storage/src/utils/index.ts
@@ -223,9 +223,6 @@ export async function validateName(
   if (!name) {
     return randomUUID();
   }
-  if (isUUID(name)) {
-    return name;
-  }
   const config = ConfigController.getInstance().config;
   const extension = path.extname(name);
   const escapedExtension = extension.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
@@ -250,10 +247,4 @@ export async function validateName(
     }
     return `${name} (${count})`;
   }
-}
-
-function isUUID(uuid: string): boolean {
-  const uuidRegex =
-    /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-([89abAB])[0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
-  return uuidRegex.test(uuid);
 }
