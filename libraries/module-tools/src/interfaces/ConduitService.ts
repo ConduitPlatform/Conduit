b114836622ea4ea152dce1fac50c99a5f84770a6
Konstantinos Kopanidis <kkopanidis@users.noreply.github.com>
2024-09-18 12:43:22 +0300

    feat(module-tools): support multi-service descriptors

--- libraries/module-tools/src/interfaces/ConduitService.ts
+++ libraries/module-tools/src/interfaces/ConduitService.ts
@@ -1,12 +1,14 @@
 import { GrpcRequest, GrpcResponse } from '@conduitplatform/grpc-sdk';
 
+export type ServiceFunction = (
+  call: GrpcRequest<any>,
+  callback: GrpcResponse<any>,
+) => void | Promise<void>;
+
 export interface ConduitService {
   readonly protoPath: string;
   readonly protoDescription: string;
   functions: {
-    [p: string]: (
-      call: GrpcRequest<any>,
-      callback: GrpcResponse<any>,
-    ) => void | Promise<void>;
+    [p: string]: ServiceFunction | { [p: string]: ServiceFunction };
   };
 }
