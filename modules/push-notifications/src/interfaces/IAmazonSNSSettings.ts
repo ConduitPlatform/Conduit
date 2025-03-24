4fa7644a28fa1f3b01b2cd75f4e8d644bd38502d
ioanniskemerlis <ioanniskemerlis@gmail.com>
2025-03-24 17:54:42 +0200

    fix: removed topic support

--- modules/push-notifications/src/interfaces/IAmazonSNSSettings.ts
+++ modules/push-notifications/src/interfaces/IAmazonSNSSettings.ts
@@ -3,5 +3,4 @@ export interface IAmazonSNSSettings {
   secretAccessKey: string;
   region: string;
   platformApplications: Record<string, string>; // Maps platform types to their SNS ARNs
-  topicArn?: string; // Optional SNS Topic for global notifications
 }
