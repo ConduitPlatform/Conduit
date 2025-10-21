bb0e8e86d5e5851e0cd80c142d38719926ff8d0c
Konstantinos Kopanidis <kkopanidis@users.noreply.github.com>
2025-10-21 17:08:27 +0300

    feat: remove forms module

--- modules/forms/src/templates/FormSubmission.ts
+++
@@ -1,6 +0,0 @@
-export const FormSubmissionTemplate = {
-  name: 'FormSubmission',
-  subject: 'Heads up! You have a new form submission!',
-  body: "Hey there, </br> Here's the reply that you got!</br>{{{data}}} ",
-  variables: ['data'],
-};
