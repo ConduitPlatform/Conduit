6355e763e674b31ba8d9eb1e9f51f15c97976e89
Konstantinos Kopanidis <kkopanidis@users.noreply.github.com>
2025-10-23 16:53:37 +0300

    feat(communications)!: new module to merge email,push and sms (wip) feat: migration cleanups

--- modules/authentication/src/migrations/index.ts
+++ modules/authentication/src/migrations/index.ts
@@ -1,17 +1,3 @@
 import { ConduitGrpcSdk } from '@conduitplatform/grpc-sdk';
-import { UserIdToUserTokenSchemaMigration } from './UserIdToUserTokenSchemaMigration.js';
-import { UserIdToUserAccessTokenSchemaMigration } from './UserIdToUserAccessTokenSchemaMigration.js';
-import { UserIdToUserRefreshTokenSchemaMigration } from './UserIdToUserRefreshTokenSchemaMigration.js';
-import { UserIdToUserTwoFactorSchemaMigration } from './UserIdToUserTwoFactorSchemaMigration.js';
 
-export async function runMigrations(grpcSdk: ConduitGrpcSdk) {
-  await UserIdToUserTokenSchemaMigration(grpcSdk).catch(ignorePostgres);
-  await UserIdToUserAccessTokenSchemaMigration(grpcSdk).catch(ignorePostgres);
-  await UserIdToUserRefreshTokenSchemaMigration(grpcSdk).catch(ignorePostgres);
-  await UserIdToUserTwoFactorSchemaMigration(grpcSdk).catch(ignorePostgres);
-}
-
-function ignorePostgres(error: { details: string }) {
-  if (error.details.endsWith('.userId does not exist')) return;
-  throw error;
-}
+export async function runMigrations(grpcSdk: ConduitGrpcSdk) {}
