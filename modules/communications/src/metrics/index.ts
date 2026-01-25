c27b1b0c8bb85ffa797a85bb6a663b926401e128
Konstantinos Kopanidis <kkopanidis@users.noreply.github.com>
2026-01-25 20:19:25 +0200

    fix(communications): metrics format change

--- modules/communications/src/metrics/index.ts
+++ modules/communications/src/metrics/index.ts
@@ -1,55 +1,57 @@
+import { MetricType } from '@conduitplatform/grpc-sdk';
+
 export default {
   email_templates_total: {
-    type: 'gauge',
+    type: MetricType.Gauge,
     config: {
       name: 'email_templates_total',
       help: 'Total number of email templates',
     },
   },
   emails_sent_total: {
-    type: 'counter',
+    type: MetricType.Counter,
     config: {
       name: 'emails_sent_total',
       help: 'Total number of emails sent',
     },
   },
   push_notifications_sent_total: {
-    type: 'counter',
+    type: MetricType.Counter,
     config: {
       name: 'push_notifications_sent_total',
       help: 'Total number of push notifications sent',
     },
   },
   sms_sent_total: {
-    type: 'counter',
+    type: MetricType.Counter,
     config: {
       name: 'sms_sent_total',
       help: 'Total number of SMS messages sent',
     },
   },
   communications_sent_total: {
-    type: 'counter',
+    type: MetricType.Counter,
     config: {
       name: 'communications_sent_total',
       help: 'Total number of communications sent',
     },
   },
   communications_success_total: {
-    type: 'counter',
+    type: MetricType.Counter,
     config: {
       name: 'communications_success_total',
       help: 'Total number of successful communications',
     },
   },
   communications_failure_total: {
-    type: 'counter',
+    type: MetricType.Counter,
     config: {
       name: 'communications_failure_total',
       help: 'Total number of failed communications',
     },
   },
   fallback_chain_used_total: {
-    type: 'counter',
+    type: MetricType.Counter,
     config: {
       name: 'fallback_chain_used_total',
       help: 'Total number of times fallback chain was used',
