5e7b5339edc9734f838b92c52381f0c5b819fa03
Ioannis <ioanniskemerlis@gmail.com>
2026-04-02 19:07:41 +0300

    fix(email): attachments(#1452)
    Co-authored-by: chris <christinapdgn@gmail.com>

--- modules/email/src/interfaces/SendEmailParams.ts
+++ modules/email/src/interfaces/SendEmailParams.ts
@@ -1,4 +1,5 @@
 import { Indexable } from '@conduitplatform/grpc-sdk';
+import { Attachment } from 'nodemailer/lib/mailer';
 
 export interface ISendEmailParams {
   email: string;
@@ -8,5 +9,5 @@ export interface ISendEmailParams {
   sender: string;
   cc?: string[];
   replyTo?: string;
-  attachments?: string[];
+  attachments?: Attachment[];
 }
